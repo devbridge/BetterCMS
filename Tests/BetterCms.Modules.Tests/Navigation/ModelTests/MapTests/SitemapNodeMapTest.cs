a576391e811731b3fb12e71aa399bf76373f2b58
Simonas Mikulenas <simonas@devbridge.com>
2013-01-07 11:26:39 +0200

    Module renaming.

--- Tests/BetterCms.Modules.Tests/Navigation/ModelTests/MapTests/SitemapNodeMapTest.cs
+++ Tests/BetterCms.Modules.Tests/Navigation/ModelTests/MapTests/SitemapNodeMapTest.cs
@@ -9,10 +9,10 @@ namespace BetterCms.Test.Module.Navigation.ModelTests.MapTests
         public void Should_Insert_And_Retrieve_SitemapNode_Successfully()
         {
             var parentNode = TestDataProvider.CreateNewSitemapNode();
-//            var childNode1 = TestDataProvider.CreateNewSitemapNode();
-//            var childNode2 = TestDataProvider.CreateNewSitemapNode();
-//            childNode1.ParentNode = parentNode;
-//            childNode2.ParentNode = parentNode;
+            var childNode1 = TestDataProvider.CreateNewSitemapNode();
+            var childNode2 = TestDataProvider.CreateNewSitemapNode();
+            childNode1.ParentNode = parentNode;
+            childNode2.ParentNode = parentNode;
             RunEntityMapTestsInTransaction(parentNode);
         }
     }
