428b464ec3904526c57aae240535b38786326169
Simonas Mikulenas <simonas@devbridge.com>
2013-01-07 11:40:45 +0200

    Not usable reference removed.

--- Modules/BetterCms.Module.Navigation/Properties/AssemblyInfo.cs
+++ Modules/BetterCms.Module.Navigation/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Web;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BetterCms.Module.Blog")]
+[assembly: AssemblyTitle("BetterCms.Module.Navigation")]
 [assembly: AssemblyDescription("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
