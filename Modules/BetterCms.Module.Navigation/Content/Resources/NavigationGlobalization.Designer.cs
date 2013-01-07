c3657da9c9bd342be1af4a8dcf05a1fda0ae1ed1
Simonas Mikulenas <simonas@devbridge.com>
2013-01-07 11:46:38 +0200

    Globalization file renamed.

--- Modules/BetterCms.Module.Navigation/Content/Resources/NavigationGlobalization.Designer.cs
+++ Modules/BetterCms.Module.Navigation/Content/Resources/NavigationGlobalization.Designer.cs
@@ -22,14 +22,14 @@ namespace BetterCms.Module.Navigation.Content.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class SitemapGlobalization {
+    public class NavigationGlobalization {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal SitemapGlobalization() {
+        internal NavigationGlobalization() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace BetterCms.Module.Navigation.Content.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BetterCms.Module.Navigation.Content.Resources.SitemapGlobalization", typeof(SitemapGlobalization).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BetterCms.Module.Navigation.Content.Resources.NavigationGlobalization", typeof(NavigationGlobalization).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
