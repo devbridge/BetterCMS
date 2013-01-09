d932baf4f9a6a128e31284d9f32e2d0c7666bac3
Audrunas Matonis <audrunas@devbridge.com>
2013-01-09 08:43:40 +0200

    #18 Added missing files

---
+++ Modules/BetterCms.Module.Blog/ViewModels/Blog/SiteSettingBlogPostViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using BetterCms.Module.Root.Mvc.Grids;
+
+namespace BetterCms.Module.Blog.ViewModels.Blog
+{
+    public class SiteSettingBlogPostViewModel : IEditableGridItem
+    {
+        /// <summary>
+        /// Gets or sets the blog id.
+        /// </summary>
+        /// <value>
+        /// The blog id.
+        /// </value>
+        public virtual Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the entity version.
+        /// </summary>
+        /// <value>
+        /// The entity version.
+        /// </value>
+        public virtual int Version { get; set; }
+
+        /// <summary>
+        /// Gets or sets the blog title.
+        /// </summary>
+        /// <value>
+        /// The blog title.
+        /// </value>
+        public virtual string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date the blog post is created on.
+        /// </summary>
+        /// <value>
+        /// The date the blog post is created on.
+        /// </value>
+        public DateTime CreatedOn { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date the blog post is modified on.
+        /// </summary>
+        /// <value>
+        /// The date the blog post is modified on.
+        /// </value>
+        public DateTime ModifiedOn { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user, name which last modified the blog post.
+        /// </summary>
+        /// <value>
+        /// The user name, which last modified the blog post.
+        /// </value>
+        public string ModifiedByUser { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether page is published.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if page is published; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsPublished { get; set; }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return string.Format("Id: {0}, Version: {1}, Title: {2}", Id, Version, Title);
+        }
+    }
+}
