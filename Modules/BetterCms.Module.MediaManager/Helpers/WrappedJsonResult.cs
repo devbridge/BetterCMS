a1537c57c30dc078b5b69143e85e2c07bd878bfb
Vytautas Naudzius <vytautas.naudzius@devbridge.com>
2013-01-30 14:41:55 +0200

    Implemented files upload logic for IE8.

---
+++ Modules/BetterCms.Module.MediaManager/Helpers/WrappedJsonResult.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace BetterCms.Module.MediaManager.Helpers
+{
+    public class WrappedJsonResult : JsonResult
+    {
+        public override void ExecuteResult(ControllerContext context)
+        {
+            context.HttpContext.Response.Write("<html><body><textarea id=\"jsonResult\" name=\"jsonResult\">");
+            base.ExecuteResult(context);
+            context.HttpContext.Response.Write("</textarea></body></html>");
+            context.HttpContext.Response.ContentType = "text/html";
+        }
+    }
+}
--- Modules/BetterCms.Module.MediaManager/Registration/MediaUploadJavaScriptModuleDescriptor.cs
+++ Modules/BetterCms.Module.MediaManager/Registration/MediaUploadJavaScriptModuleDescriptor.cs
@@ -22,8 +22,8 @@ namespace BetterCms.Module.MediaManager.Registration
                 {    
                     new JavaScriptModuleLinkTo<UploadController>(this, "loadUploadFilesDialogUrl", f => f.MultiFileUpload("{0}", "{1}")),
                     new JavaScriptModuleLinkTo<UploadController>(this, "uploadFileToServerUrl", f => f.UploadMedia(null)),
-                    new JavaScriptModuleLinkTo<UploadController>(this, "undoFileUploadUrl", f => f.RemoveFileUpload("{0}", "{1}", "{2}"))
-                    
+                    new JavaScriptModuleLinkTo<UploadController>(this, "undoFileUploadUrl", f => f.RemoveFileUpload("{0}", "{1}", "{2}")),
+                    new JavaScriptModuleLinkTo<UploadController>(this, "loadUploadSingleFileDialogUrl", f => f.SingleFileUpload("{0}", "{1}"))
                 };
 
             Globalization = new IActionProjection[]
