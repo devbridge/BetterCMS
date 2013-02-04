6aa4bc1b2f8245df1f3e27498587021dd9470bce
Simonas Mikulenas <simonas@devbridge.com>
2013-02-04 09:36:57 +0200

    #128: initial commit for NuGet package creation.

---
+++ Build/BetterCms.NugetPackage/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace BetterCms.NugetPackage
+{
+    /// <summary>
+    /// Fake program to force solution to build NuGet package.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Mains the specified args.
+        /// </summary>
+        /// <param name="args">The args.</param>
+        public static void Main(string[] args)
+        {
+            var nugetTemplateFile = args[0];    // BetterCMS.nuspec.template;
+            var nugetFile = args[1];            // BetterCMS.nuspec;
+
+            const string sufix = "-pre";
+            var version = Assembly.GetEntryAssembly().GetName().Version;
+            var textVersion = string.Format("{0}.{1}.{2}{3}", version.Major, version.Minor, version.Build, sufix);
+
+            using (StreamReader sr = new StreamReader(nugetTemplateFile))
+            {
+                try
+                {
+                    var templateFile = sr.ReadToEnd();
+                    templateFile = templateFile.Replace("@BetterCMSVersion@", textVersion);
+
+                    using (StreamWriter sw = new StreamWriter(nugetFile))
+                    {
+                        try
+                        {
+                            sw.Write(templateFile);
+                        }
+                        finally
+                        {
+                            sw.Close();
+                        }
+                    }
+                }
+                finally
+                {
+                    sr.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Build/BetterCms.NugetPackage/package/tools/install.ps1
@@ -0,0 +1,24 @@
+param($installPath, $toolsPath, $package, $project)
+
+# Add reference.
+$project.Object.References | Where-Object { $_.Name -eq 'FluentMigrator.Runner' } | ForEach-Object { $_.Remove() }
+$project.Object.References.Add("$installPath\..\FluentMigrator.1.0.6.0\tools\FluentMigrator.Runner.dll")
+
+# Copy config file.
+$path = [System.IO.Path]
+$source = $path::Combine($path::GetDirectoryName($project.FileName), "Views\Web.config")
+$destination = $path::Combine($path::GetDirectoryName($project.FileName), "Areas\Web.config")
+$destinationFolder = $path::Combine($path::GetDirectoryName($project.FileName), "Areas")
+
+if (!(Test-Path $destination))
+{
+    if (Test-Path $source)
+    {
+        if(!(Test-Path $destinationFolder))
+        {
+            New-Item $destinationFolder -type directory
+        }
+        Copy-Item $source $destination
+        $project.ProjectItems.AddFromFile($destination)
+    }
+}
