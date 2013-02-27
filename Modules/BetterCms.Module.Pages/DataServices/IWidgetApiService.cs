3f593d83153e875d880ce927103bd37b8d1a549d
Simonas Mikulenas <simonas@devbridge.com>
2013-02-27 16:36:51 +0200

    #31: implemented widgets API service.

--- Modules/BetterCms.Module.Pages/DataServices/IWidgetApiService.cs
+++ Modules/BetterCms.Module.Pages/DataServices/IWidgetApiService.cs
@@ -1,6 +1,20 @@
-﻿namespace BetterCms.Module.Pages.DataServices
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+using BetterCms.Module.Pages.Models;
+using BetterCms.Module.Root.Models;
+
+namespace BetterCms.Module.Pages.DataServices
 {
     public interface IWidgetApiService
     {
+        HtmlContentWidget GetHtmlContentWidget(Guid id);
+
+        ServerControlWidget GetServerControlWidget(Guid id);
+
+        IList<Widget> GetWidgets(Expression<Func<Widget, bool>> filter = null, Expression<Func<Widget, dynamic>> order = null, bool orderDescending = false, int? pageNumber = null, int? itemsPerPage = null);
+
+        IList<Widget> GetPageWidgets(Guid pageId, Expression<Func<Widget, bool>> filter = null);
     }
 }
--- Tests/BetterCms.Modules.Tests/Api/ApiTests.cs
+++ Tests/BetterCms.Modules.Tests/Api/ApiTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 
 using BetterCms.Core.DataAccess;
 using BetterCms.Core.DataAccess.DataContext;
@@ -28,22 +29,27 @@ namespace BetterCms.Test.Module.Api
                 var repository = new DefaultRepository(unitOfWork);
                 var service = new DefaultContentApiService(repository);
 
-                var pageId = new Guid("B0326B23-D0C0-4B4A-B7EE-A17200AE46BE");
-//
+                var pageId = new Guid("66d2dc73-9908-4a54-ad6b-a15f00c50f91");
+
 //                var contents1 = service.GetPageContents(pageId, p => ((PageProperties)p.Page).CustomCss.Transliterate(true) == "agfa");
 //                var contents2 = service.GetRegionContents(pageId, contents1[0].Region.Id);
 //                var contents3 = service.GetRegionContents(pageId, contents1[0].Region.RegionIdentifier);
 //                var content = service.GetContent(contents1[0].Content.Id);
 //                var pageContent = service.GetPageContent(contents1[0].Id);
 
-                //var result = service.GetFile(folder.Id);
+//                var result = service.GetFile(folder.Id);
 
 
 
-
-
-
-
+//                var widgetApi = new DefaultWidgetApiService(repository);
+//                var widgets = widgetApi.GetPageWidgets(pageId);
+//                var result = new StringBuilder("");
+//                foreach (var widget in widgets)
+//                {
+//                    result.AppendLine(string.Format("{0}: {1}, page options count: {2}", widget.Name, widget.Category != null ? widget.Category.Name : "[unknown category]", widget.PageContents.Where(p => p.Page.Id == pageId).Sum(p => p.Options.Count)));
+//                }
+//                var x = result.ToString();
+                
 //                var test = contents1;
             });
         }
