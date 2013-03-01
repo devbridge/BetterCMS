0f5ec7fd37102965ac36af6cec0a07f61634a350
Vytautas Naudzius <vytautas.naudzius@devbridge.com>
2013-03-01 15:22:10 +0200

    #24 implemented success message showing.

--- Modules/BetterCms.Module.Users/UsersConstants.cs
+++ Modules/BetterCms.Module.Users/UsersConstants.cs
@@ -2,8 +2,8 @@
 {
     public class UsersConstants
     {
-        public const string EmailRegularExpression = "TODO";
+        public const string EmailRegularExpression = @"^[a-zA-Z0-9_\+-]+(\.[a-zA-Z0-9_\+-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.([a-zA-Z]{2,4})$";
 
-        public const string PasswordRegularExpression = "TODO";
+        public const string PasswordRegularExpression = @"^.{4}(.{255})?$";
     }
 }
