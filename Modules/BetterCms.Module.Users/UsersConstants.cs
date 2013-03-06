04d3852fb24d63e0564a1260e5380d20dccf8545
Simonas Mikulenas <simonas@devbridge.com>
2013-03-06 14:39:15 +0200

    Unused file removed.

--- Modules/BetterCms.Module.Users/UsersConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BetterCms.Module.Users
-{
-    public class UsersConstants
-    {
-        public const string EmailRegularExpression = @"^[a-zA-Z0-9_\+-]+(\.[a-zA-Z0-9_\+-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.([a-zA-Z]{2,4})$";
-
-        public const string PasswordRegularExpression = @"^.{4}(.{255})?$";
-    }
-}
