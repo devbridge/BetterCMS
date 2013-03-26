eb2e9efb86ea0286cf70f97e2b348328149cd779
Audrunas Matonis <audrunas@devbridge.com>
2013-03-26 11:25:13 -0500

    #342 Created pageStatuses table, fixed creating of new page

---
+++ Modules/BetterCms.Module.Root/Models/Migrations/Migration201303261100.cs
@@ -0,0 +1,92 @@
+﻿using BetterCms.Core.DataAccess.DataContext.Migrations;
+using BetterCms.Core.Models;
+
+using FluentMigrator;
+
+namespace BetterCms.Module.Root.Models.Migrations
+{
+    [Migration(201303261100)]
+    public class Migration201303261100 : DefaultMigration
+    {
+        public Migration201303261100()
+            : base(RootModuleDescriptor.ModuleName)
+        {
+        }
+
+        /// <summary>
+        /// Migrate up.
+        /// </summary>
+        public override void Up()
+        {
+            // Set non valid statuses as unpublished
+            Update
+                .Table("Pages").InSchema(SchemaName)
+                .Set(new { Status = 4 })
+                .Where(new { Status = 0 });
+
+            // Create table
+            Create
+                .Table("PageStatuses")
+                .InSchema(SchemaName)
+                .WithColumn("Id").AsInt32().PrimaryKey()
+                .WithColumn("Name").AsString(MaxLength.Name).NotNullable();
+
+            // Create Uq
+            Create
+                .UniqueConstraint("UX_Cms_PageStatuses_Name")
+                .OnTable("PageStatuses").WithSchema(SchemaName)
+                .Column("Name");
+
+            // Insert page statuses
+            Insert
+                .IntoTable("PageStatuses")
+                .InSchema(SchemaName)
+                .Row(new
+                {
+                    Id = 1,
+                    Name = "Preview"
+                })
+                .Row(new
+                {
+                    Id = 2,
+                    Name = "Draft"
+                })
+                .Row(new
+                {
+                    Id = 3,
+                    Name = "Published"
+                })
+                .Row(new
+                {
+                    Id = 4,
+                    Name = "Unpublished"
+                });
+
+            // Create FK
+            Create
+                .ForeignKey("FK_Cms_RootPages_Cms_PageStatuses")
+                .FromTable("Pages").InSchema(SchemaName).ForeignColumn("Status")
+                .ToTable("PageStatuses").InSchema(SchemaName).PrimaryColumn("Id");
+        }
+
+        /// <summary>
+        /// Migrate down.
+        /// </summary>
+        public override void Down()
+        {
+            // Delete FK
+            Delete
+                .ForeignKey("FK_Cms_RootPages_Cms_PageStatuses")
+                .OnTable("Pages").InSchema(SchemaName);
+
+            // Delete unique constraint
+            Delete
+                .UniqueConstraint("UX_Cms_PageStatuses_Name")
+                .FromTable("PageStatuses").InSchema(SchemaName);
+
+            // Delete table
+            Delete
+                .Table("PageStatuses").InSchema(SchemaName);
+        }
+    }
+}
