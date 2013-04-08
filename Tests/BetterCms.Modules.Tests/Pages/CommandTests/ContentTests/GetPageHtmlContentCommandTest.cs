116190e617bc28d221546de19c588585e27feea4
Audrunas Matonis <audrunas@devbridge.com>
2013-04-08 14:23:39 -0500

    Created some command tests for Pages Module

---
+++ Tests/BetterCms.Modules.Tests/Pages/CommandTests/ContentTests/GetPageHtmlContentCommandTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+using Autofac;
+
+using BetterCms.Core.DataAccess;
+using BetterCms.Core.DataAccess.DataContext;
+using BetterCms.Core.DataContracts.Enums;
+using BetterCms.Module.Root.Models;
+using BetterCms.Module.Root.Services;
+using BetterCms.Module.Pages.Command.Content.GetPageHtmlContent;
+
+using Moq;
+
+using NHibernate;
+
+using NUnit.Framework;
+
+namespace BetterCms.Test.Module.Pages.CommandTests.ContentTests
+{
+    [TestFixture]
+    public class GetPageHtmlContentCommandTest : TestBase
+    {
+        [Test]
+        public void Should_Return_Page_Html_Content()
+        {
+            // Create html content
+            var htmlContent = TestDataProvider.CreateNewHtmlContent();
+            var pageContent = TestDataProvider.CreateNewPageContent(htmlContent);
+            htmlContent.Status = ContentStatus.Published;
+
+            // Create command
+            var command = new GetPageHtmlContentCommand();
+            command.UnitOfWork = new Mock<IUnitOfWork>().Object;
+            command.Repository = new Mock<IRepository>().Object;
+                
+            // Mock content service
+            var serviceMock = new Mock<IContentService>();
+            serviceMock
+                .Setup(f => f.GetPageContentForEdit(pageContent.Id))
+                .Returns(new Tuple<PageContent, Content>(pageContent, htmlContent));
+            command.ContentService = serviceMock.Object;
+
+            // Execute command
+            var result = command.Execute(pageContent.Id);                     
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Id, pageContent.Id);
+            Assert.AreEqual(result.ContentId, htmlContent.Id);
+            Assert.AreEqual(result.ContentName, htmlContent.Name);
+            Assert.AreEqual(result.ContentVersion, htmlContent.Version);
+            Assert.AreEqual(result.CurrentStatus, htmlContent.Status);
+            Assert.AreEqual(result.PageId, pageContent.Page.Id);
+            Assert.AreEqual(result.RegionId, pageContent.Region.Id);
+            Assert.AreEqual(result.LiveFrom, htmlContent.ActivationDate);
+            Assert.AreEqual(result.LiveTo, htmlContent.ExpirationDate);
+        }
+    }
+}
