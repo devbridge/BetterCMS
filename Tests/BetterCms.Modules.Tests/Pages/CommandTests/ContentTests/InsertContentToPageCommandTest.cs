7d6c735f6cbe8dafb7a01b014e9c13aa474d9746
Audrunas Matonis <audrunas@devbridge.com>
2013-04-08 15:44:00 -0500

    Created command tests for Pages Module

---
+++ Tests/BetterCms.Modules.Tests/Pages/CommandTests/ContentTests/InsertContentToPageCommandTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using BetterCms.Core.DataAccess;
+using BetterCms.Core.DataAccess.DataContext;
+using BetterCms.Core.DataContracts.Enums;
+using BetterCms.Core.Models;
+using BetterCms.Module.Pages.Command.Content.InsertContent;
+using BetterCms.Module.Root.Models;
+
+using Moq;
+
+using NUnit.Framework;
+
+namespace BetterCms.Test.Module.Pages.CommandTests.ContentTests
+{
+    [TestFixture]
+    public class InsertContentToPageCommandTest : TestBase
+    {
+        [Test]
+        public void Should_Insert_HtmlWidget_To_Page_Successfully()
+        {
+            // Create html content
+            var widget = TestDataProvider.CreateNewHtmlContentWidget();
+            widget.Status = ContentStatus.Published;
+
+            var page = TestDataProvider.CreateNewPage();
+            var region = TestDataProvider.CreateNewRegion();
+            var pageContent = TestDataProvider.CreateNewPageContent(widget, page, region);
+            pageContent.Order = TestDataProvider.ProvideRandomNumber(1, 99999);
+
+            // Mock
+            var repository = new Mock<IRepository>();
+            repository
+                .Setup(r => r.AsProxy<Page>(It.IsAny<Guid>()))
+                .Returns(TestDataProvider.CreateNewPage());
+            repository
+                .Setup(r => r.AsProxy<Region>(It.IsAny<Guid>()))
+                .Returns(TestDataProvider.CreateNewRegion());
+            repository
+                .Setup(r => r.AsQueryable<PageContent>())
+                .Returns(new List<PageContent> { pageContent }.AsQueryable());
+            repository
+                .Setup(r => r.Save(It.IsAny<PageContent>()))
+                .Callback<PageContent>(pc =>
+                              {
+                                  Assert.AreEqual(pc.Order, pageContent.Order + 1);
+                              });
+
+            // Create command
+            var command = new InsertContentToPageCommand();
+            command.UnitOfWork = new Mock<IUnitOfWork>().Object;
+            command.Repository = repository.Object;
+
+            // Execute
+            var request = new InsertContentToPageRequest { ContentId = widget.Id, PageId = page.Id, RegionId = region.Id };
+            var result = command.Execute(request);
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ Tests/BetterCms.Modules.Tests/Pages/CommandTests/ContentTests/SavePageContentOptionsCommandTest.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using BetterCms.Core.DataAccess;
+using BetterCms.Core.DataAccess.DataContext;
+using BetterCms.Module.Pages.Command.Content.SavePageContentOptions;
+using BetterCms.Module.Pages.ViewModels.Content;
+using BetterCms.Module.Root.Models;
+
+using NUnit.Framework;
+
+namespace BetterCms.Test.Module.Pages.CommandTests.ContentTests
+{
+    [TestFixture]
+    public class SavePageContentOptionsCommandTest : GetPageContentOptionsTest
+    {
+        [Test]
+        public void Should_Save_Page_Content_Options_Successfully()
+        {
+            RunActionInTransaction(session =>
+            {
+                // Create page content with options
+                var content = TestDataProvider.CreateNewContent();
+                var pageContent = TestDataProvider.CreateNewPageContent(content);
+                FillContentWithOptions(content);
+                FillPageContentWithOptions(content, pageContent);
+
+                session.SaveOrUpdate(pageContent);
+                session.Flush();
+                session.Clear();
+
+                // Random option value
+                var randomOptionValue = TestDataProvider.CreateNewPageContentOption();
+
+                // Create request
+                var request = new PageContentOptionsViewModel
+                    {
+                        WidgetOptions = new List<PageContentOptionViewModel>
+                            {
+                                  new PageContentOptionViewModel
+                                      {
+                                          // Will be deleted because of default value
+                                          OptionValue = content.ContentOptions[0].DefaultValue,
+                                          OptionKey = pageContent.Options[0].Key,
+                                          OptionDefaultValue = content.ContentOptions[0].DefaultValue
+                                      },
+                                  new PageContentOptionViewModel
+                                      {
+                                          // Will be deleted because of null value
+                                          OptionValue = null,
+                                          OptionKey = pageContent.Options[1].Key
+                                      },
+                                  new PageContentOptionViewModel
+                                      {
+                                          OptionValue = pageContent.Options[2].Value,
+                                          OptionKey = pageContent.Options[2].Key
+                                      },
+                                  new PageContentOptionViewModel
+                                      {
+                                          // Random value
+                                          OptionValue = randomOptionValue.Value,
+                                          OptionKey = randomOptionValue.Key
+                                      }
+
+                            },
+                        PageContentId = pageContent.Id
+                    };
+
+                // Create command
+                var unitOfWork = new DefaultUnitOfWork(session);
+                var repository = new DefaultRepository(unitOfWork);
+                var command = new SavePageContentOptionsCommand();
+                command.UnitOfWork = unitOfWork;
+                command.Repository = repository;
+                var result = command.Execute(request);
+
+                Assert.IsTrue(result);
+
+                // Check results: one of page content values must be deleted after save
+                var results = repository
+                    .AsQueryable<PageContentOption>(pco => pco.PageContent.Id == pageContent.Id
+                        && !pco.IsDeleted
+                        && !pco.PageContent.IsDeleted)
+                    .ToList();
+                Assert.AreEqual(results.Count, 2);
+                Assert.IsNotNull(results.FirstOrDefault(pco => pco.Key == pageContent.Options[2].Key
+                    && pco.Value == pageContent.Options[2].Value));
+                Assert.IsNotNull(results.FirstOrDefault(pco => pco.Key == randomOptionValue.Key
+                    && pco.Value == randomOptionValue.Value));
+            });
+        }
+    }
+}
