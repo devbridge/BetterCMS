98016363442926ae70fdc16e1ae63417dabf1db0
Audrunas Matonis <audrunas@devbridge.com>
2013-05-17 16:48:22 +0300

    #358 reverted sandbox changes

--- Sandbox/BetterCms.Sandbox.Mvc4/Controllers/SandboxController.cs
+++ Sandbox/BetterCms.Sandbox.Mvc4/Controllers/SandboxController.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using System.Web.UI.WebControls;
 
 using BetterCms.Api;
 using BetterCms.Core;
+using BetterCms.Module.MediaManager.Models;
+using BetterCms.Module.Pages.Api.Events;
 using BetterCms.Module.Pages.Models;
-using BetterCms.Module.Root.Models;
+
 using BetterCms.Sandbox.Mvc4.Models;
 
 namespace BetterCms.Sandbox.Mvc4.Controllers
@@ -15,7 +22,7 @@ namespace BetterCms.Sandbox.Mvc4.Controllers
     public class SandboxController : Controller
     {
         public ActionResult Content()
-        {                
+        {
             return Content("Hello from the web project controller.");
         }
 
@@ -32,7 +39,7 @@ namespace BetterCms.Sandbox.Mvc4.Controllers
         [AllowAnonymous]
         public ActionResult Login(string roles)
         {
-//            var roles = string.Join(",", Roles.GetRolesForUser(string.Empty));
+            //            var roles = string.Join(",", Roles.GetRolesForUser(string.Empty));
             if (string.IsNullOrEmpty(roles))
             {
                 roles = "Owner";
@@ -63,7 +70,7 @@ namespace BetterCms.Sandbox.Mvc4.Controllers
 
         public ActionResult TestApi()
         {
-            /*PagesApiContext.Events.PageCreated += EventsOnPageCreated ;
+            PagesApiContext.Events.PageCreated += EventsOnPageCreated;
 
             PagesApiContext.Events.OnPageCreated(new PageProperties());
 
@@ -71,7 +78,7 @@ namespace BetterCms.Sandbox.Mvc4.Controllers
 
             IList<MediaFolder> folders;
             using (var mediaApi = CmsContext.CreateApiContextOf<MediaManagerApiContext>())
-            {                
+            {
                 folders = mediaApi.GetFolders(MediaType.Image);
             }
 
@@ -83,41 +90,7 @@ namespace BetterCms.Sandbox.Mvc4.Controllers
                 message = string.Format("{0}<br /> Image folders titles: {1}", message, string.Join("; ", folders.Select(t => t.Title)));
             }
 
-            return Content(message);*/
-
-            ServerControlWidget result;
-
-            
-            using (var pagesApi = CmsContext.CreateApiContextOf<PagesApiContext>())
-            {
-                using (var transaction = new System.Transactions.TransactionScope())
-                {
-                    // pagesApi.CreateLayout(/* parameters */);
-                    // result = pagesApi.CreateLayoutRegion(new Guid("8E954FBD-1550-4EEC-B730-A1C100EC25D3"), region);
-                    // pagesApi.CreateHtmlContentWidget(/* parameters */);
-
-                    string name = "API Server Widget";
-                    string path = "~/Views/Widgets/01.cshtml";
-                    var categoryId = new Guid("A4662051-CE31-431F-8B56-A1B200F63C21");
-                    var previewUrl = "http://www.yahoo.com";
-
-                    /*string css = "";
-                    string javaScript = "";
-                    string html = "<div class='aaa'>HTML CONTENT GOES HERE</div>";
-                    Guid categoryId = new Guid("A4662051-CE31-431F-8B56-A1B200F63C21");*/
-
-                    result = pagesApi.CreateServerControlWidget(name, path, categoryId, previewUrl);
-
-                    pagesApi.CreateContentOption(result.Id, "Option 1", "def Value 1");
-                    pagesApi.CreateContentOption(result.Id, "Option 2", "def Value 2");
-
-                    transaction.Complete();
-                }
-            }
-           
-            
-
-            return Content(result.Id.ToString());
+            return Content(message);
         }
 
         void Core_HostStart(SingleItemEventArgs<HttpApplication> args)
@@ -127,16 +100,16 @@ namespace BetterCms.Sandbox.Mvc4.Controllers
 
         private void EventsOnPageCreated(SingleItemEventArgs<PageProperties> args)
         {
-            
+
         }
 
         public ActionResult TestNavigationApi()
         {
-            
-            var message = new StringBuilder("No sitemap data found!");                     
+
+            var message = new StringBuilder("No sitemap data found!");
 
             return Content(message.ToString());
-        }        
+        }
 
         [AllowAnonymous]
         public ActionResult LoginJson(LoginViewModel login)
