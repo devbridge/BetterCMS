a210180686d63282527f61030e219449f25141de
Simonas Mikulenas <simonas@devbridge.com>
2013-05-22 14:47:42 +0300

    #446: refactoring templates module.

---
+++ BetterCms.Configuration/ICmsInstallationConfiguration.cs
@@ -0,0 +1,32 @@
+﻿namespace BetterCms
+{
+    /// <summary>
+    /// Installation module configuration contract.
+    /// </summary>
+    public interface ICmsInstallationConfiguration
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether to install 404 error page.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> to install 404 error page; otherwise, <c>false</c>.
+        /// </value>
+        bool Install404ErrorPage { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to install 500 error page.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> to install 500 error page; otherwise, <c>false</c>.
+        /// </value>
+        bool Install500ErrorPage { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to install default page.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> to install default page; otherwise, <c>false</c>.
+        /// </value>
+        bool InstallDefaultPage { get; set; }
+    }
+}
---
+++ Modules/BetterCms.Module.Installation/Models/Migrations/InitialSetup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using BetterCms.Core.DataAccess.DataContext.Migrations;
+
+using FluentMigrator;
+
+namespace BetterCms.Module.Installation.Models.Migrations
+{
+    /// <summary>
+    /// Module initial database structure creation.
+    /// </summary>
+    [Migration(201301151849)]
+    public class InitialSetup : DefaultMigration
+    {
+        private const string LayoutsTableName = "Layouts";
+        private readonly string rootSchemaName = "bcms_root";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InitialSetup"/> class.
+        /// </summary>
+        public InitialSetup() : base(InstallationModuleDescriptor.ModuleName)
+        {
+            rootSchemaName = (new Root.Models.Migrations.RootVersionTableMetaData()).SchemaName;
+        }
+
+        /// <summary>
+        /// Ups this instance.
+        /// </summary>
+        public override void Up()
+        {
+            // For backward compatibility.
+            Update.Table(LayoutsTableName)
+                  .InSchema(rootSchemaName)
+                  .Set(new { LayoutPath = "~/Areas/bcms-installation/Views/Shared/WideLayout.cshtml" })
+                  .Where(new { LayoutPath = "~/Areas/bcms-templates/Views/Shared/WideLayout.cshtml" });
+
+            Update.Table(LayoutsTableName)
+                  .InSchema(rootSchemaName)
+                  .Set(new { LayoutPath = "~/Areas/bcms-installation/Views/Shared/TwoColumnsLayout.cshtml" })
+                  .Where(new { LayoutPath = "~/Areas/bcms-templates/Views/Shared/TwoColumnsLayout.cshtml" });
+
+            Update.Table(LayoutsTableName)
+                  .InSchema(rootSchemaName)
+                  .Set(new { LayoutPath = "~/Areas/bcms-installation/Views/Shared/ThreeColumnsLayout.cshtml" })
+                  .Where(new { LayoutPath = "~/Areas/bcms-templates/Views/Shared/ThreeColumnsLayout.cshtml" });
+        }
+
+        /// <summary>
+        /// Downs this instance.
+        /// </summary>
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
