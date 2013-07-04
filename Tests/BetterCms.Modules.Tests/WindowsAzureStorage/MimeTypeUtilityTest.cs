0c7fa1410ab49b9d2f8bdcf5ca67c3105379c6d0
Tomas Kirda <tomas@devbridge.com>
2013-07-04 10:45:16 -0500

    Determine and set ContentType for files that are uploaded to Azure storage.

---
+++ Tests/BetterCms.Modules.Tests/WindowsAzureStorage/MimeTypeUtilityTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using BetterCms.Module.WindowsAzureStorage;
+
+using NUnit.Framework;
+
+namespace BetterCms.Test.Module.WindowsAzureStorage
+{
+    [TestFixture]
+    public class MimeTypeUtilityTest
+    {
+        [Test]
+        public void Should_Determine_ContentType()
+        {
+            var uri = new Uri("http://www.bettercms.com/test/logo.png");
+            var contentType = MimeTypeUtility.DetermineContentType(uri);
+
+            Assert.AreEqual("image/png", contentType);
+        }
+
+        [Test]
+        public void Should_Determine_ContentType_Uppercase()
+        {
+            var uri = new Uri("http://www.bettercms.com/test/LOGO.PNG");
+            var contentType = MimeTypeUtility.DetermineContentType(uri);
+
+            Assert.AreEqual("image/png", contentType);
+        }
+
+        [Test]
+        public void Should_Return_Default_ContentType()
+        {
+            var uri = new Uri("http://www.bettercms.com/test/logo");
+            var contentType = MimeTypeUtility.DetermineContentType(uri);
+
+            Assert.AreEqual("application/octet-stream", contentType);
+        }
+    }
+}
