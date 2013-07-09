02ad947ec699af23d3d4e54d8207a6426b81873d
Simonas Mikulenas <simonas@devbridge.com>
2013-07-09 17:48:50 +0300

    #529: WidgetViewModel added for server widgets views.

---
+++ Modules/BetterCms.Module.Root/ViewModels/Cms/WigetViewModel.cs
@@ -0,0 +1,11 @@
+﻿using BetterCms.Core.DataContracts;
+
+namespace BetterCms.Module.Root.ViewModels.Cms
+{
+    public class WigetViewModel
+    {
+        public IPage Page { get; set; }
+
+        public IWidget Widget { get; set; }
+    }
+}
