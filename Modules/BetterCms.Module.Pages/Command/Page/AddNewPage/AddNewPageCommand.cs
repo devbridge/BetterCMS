064bc906985e32dbf1cff5cb52475420f9b26a61
Tomas Kirda <tomas@devbridge.com>
2013-08-13 09:39:21 +0300

    Update AddNewPageCommand to include access control settings.

--- Modules/BetterCms.Module.Pages/Command/Page/AddNewPage/AddNewPageCommand.cs
+++ Modules/BetterCms.Module.Pages/Command/Page/AddNewPage/AddNewPageCommand.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 
 using BetterCms.Core.Mvc.Commands;
+using BetterCms.Core.Security;
 using BetterCms.Module.Pages.Services;
 using BetterCms.Module.Pages.ViewModels.Page;
 using BetterCms.Module.Root.Mvc;
@@ -9,12 +10,39 @@ namespace BetterCms.Module.Pages.Command.Page.AddNewPage
 {
     public class AddNewPageCommand : CommandBase, ICommand<AddNewPageCommandRequest, AddNewPageViewModel>
     {
-        public ILayoutService LayoutService { get; set; }
+        private readonly ILayoutService layoutService;
 
+        private readonly ICmsConfiguration cmsConfiguration;
+
+        private readonly IAccessControlService accessControlService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AddNewPageCommand"/> class.
+        /// </summary>
+        /// <param name="LayoutService">The layout service.</param>
+        /// <param name="cmsConfiguration">The CMS configuration.</param>
+        /// <param name="accessControlService">The access control service.</param>
+        public AddNewPageCommand(ILayoutService LayoutService, ICmsConfiguration cmsConfiguration, IAccessControlService accessControlService)
+        {
+            layoutService = LayoutService;
+            this.cmsConfiguration = cmsConfiguration;
+            this.accessControlService = accessControlService;
+        }
+
+        /// <summary>
+        /// Executes the specified request.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns>AddNewPage view model</returns>
         public AddNewPageViewModel Execute(AddNewPageCommandRequest request)
         {
-            var model = new AddNewPageViewModel { ParentPageUrl = request.ParentPageUrl };
-            model.Templates = LayoutService.GetTemplates();
+            var model = new AddNewPageViewModel
+                            {
+                                ParentPageUrl = request.ParentPageUrl,
+                                Templates = layoutService.GetTemplates(),
+                                AccessControlEnabled = cmsConfiguration.AccessControlEnabled,
+                                UserAccessList = accessControlService.GetDefaultAccessList()
+                            };
 
             if (model.Templates.Count > 0)
             {
