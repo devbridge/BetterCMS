9019895fe11a6324c885a293c0f956fe731d82bb
Audrunas Matonis <audrunas@devbridge.com>
2013-08-14 08:54:59 +0300

    Fixed blog posts update SQL script

--- Modules/BetterCms.Module.Blog/Models/Migrations/Migration201305271258.sqlserver.sql
+++ Modules/BetterCms.Module.Blog/Models/Migrations/Migration201305271258.sqlserver.sql
@@ -7,10 +7,12 @@ FROM bcms_blog.BlogPosts
 		LEFT JOIN bcms_root.Contents ON (bcms_root.Contents.Id = bcms_root.PageContents.ContentId and bcms_root.Contents.IsDeleted <> 1)
 		LEFT JOIN bcms_pages.HtmlContents ON (bcms_pages.HtmlContents.Id = bcms_root.Contents.Id)
 
-WHERE bcms_blog.BlogPosts.ActivationDate != bcms_pages.HtmlContents.ActivationDate	 OR
+WHERE (bcms_blog.BlogPosts.ActivationDate != bcms_pages.HtmlContents.ActivationDate	 OR
 	  bcms_blog.BlogPosts.ActivationDate IS NULL AND bcms_pages.HtmlContents.ActivationDate IS NOT NULL OR
 	  bcms_blog.BlogPosts.ActivationDate IS NOT NULL AND bcms_pages.HtmlContents.ActivationDate IS NULL OR
 	  
       bcms_blog.BlogPosts.ExpirationDate != bcms_pages.HtmlContents.ExpirationDate OR
 	  bcms_blog.BlogPosts.ExpirationDate IS NULL AND bcms_pages.HtmlContents.ExpirationDate IS NOT NULL OR
-	  bcms_blog.BlogPosts.ExpirationDate IS NOT NULL AND bcms_pages.HtmlContents.ExpirationDate IS NULL
+	  bcms_blog.BlogPosts.ExpirationDate IS NOT NULL AND bcms_pages.HtmlContents.ExpirationDate IS NULL)
+	  
+	  AND bcms_pages.HtmlContents.Id IS NOT NULL
