6b3024d71cd57fb9f3152c1eb7463d59f72f9edf
Audrunas Matonis <audrunas@devbridge.com>
2013-09-02 09:45:11 +0300

    #619 Options grid: fixed date pickers mouse down event handling

--- Modules/BetterCms.Module.Root/Scripts/bcms.options.js
+++ Modules/BetterCms.Module.Root/Scripts/bcms.options.js
@@ -97,24 +97,38 @@ bettercms.define('bcms.options', ['bcms.jquery', 'bcms', 'bcms.ko.extenders', 'b
             return OptionValuesListViewModel;
         })(OptionsListViewModel);
 
-        function attachDatePickers(self) {
+        function onDatePickerBoxFocused(self) {
             var i,
                 item,
-                len, 
-                datePickerBox = $(selectors.datePickerBox),
+                len;
+
+            for (i = 0, len = self.items().length; i < len; i++) {
+                item = self.items()[i];
+
+                if (item.isActive()) {
+                    console.log('selecting an item');
+                    item.isSelected = true;
+                }
+            }
+        }
+
+        function attachDatePickers(self) {
+            var datePickerBox = $(selectors.datePickerBox),
                 dataKey = 'bcmsEventsAttached';
 
             if (!datePickerBox.data(dataKey)) {
+                console.log('has not yet datepicker');
                 datePickerBox.data(dataKey, true);
 
                 $(selectors.datePickerBox).on('click', function () {
-                    for (i = 0, len = self.items().length; i < len; i++) {
-                        item = self.items()[i];
-                        
-                        if (item.isActive()) {
-                            item.isSelected = true;
-                        }
-                    }
+                    console.log('Picker DIV mouse downed');
+                    onDatePickerBoxFocused(self);
+                    return false;
+                });
+
+                $(selectors.datePickerBox).mousedown(function(event) {
+                    bcms.stopEventPropagation(event);
+                    return false;
                 });
             }
         }
@@ -217,11 +231,18 @@ bettercms.define('bcms.options', ['bcms.jquery', 'bcms', 'bcms.ko.extenders', 'b
                         datePickers = row.find(selectors.datePickers);
 
                     if (!datePickers.hasClass(classes.hasDatePicker)) {
+                        console.log('init element date picker');
                         datePickers.initializeDatepicker(globalization.datePickerTooltipTitle, datePickerOpts);
 
                         datePickerTrigger = row.find(selectors.datePickerTrigger);
-                        datePickerTrigger.on('click', self.onItemSelect);
-                        datePickerTrigger.on('focus', self.onItemSelect);
+                        datePickerTrigger.on('click', function (event) {
+                            console.log('Clicked element');
+                            self.onItemSelect(self, event);
+                        });
+                        datePickerTrigger.on('focus', function (event) {
+                            console.log('Focused element');
+                            self.onItemSelect(self, event);
+                        });
                     }
                 };
                 
