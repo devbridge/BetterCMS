5cd54865436aaebeb839897f04af9793ccc13ce8
Audrunas Matonis <audrunas@devbridge.com>
2013-09-04 08:04:28 +0300

    #613 ToString() added to media manager view model

--- Modules/BetterCms.Module.MediaManager/ViewModels/MediaManager/MediaManagerIndexViewModel.cs
+++ Modules/BetterCms.Module.MediaManager/ViewModels/MediaManager/MediaManagerIndexViewModel.cs
@@ -5,5 +5,16 @@ namespace BetterCms.Module.MediaManager.ViewModels.MediaManager
     public class MediaManagerIndexViewModel : MediaViewModel
     {
         public UserMessages CustomFilesMessages { get; set; }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return string.Format("{0}, CustomFilesMessages: {1}", base.ToString(), CustomFilesMessages != null);
+        }
     }
 }
