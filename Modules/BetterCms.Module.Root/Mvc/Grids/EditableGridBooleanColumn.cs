1e7f262bdda3fb48d1a9301b472b2a0e21f1b21c
Audrunas Matonis <audrunas@devbridge.com>
2013-09-06 07:44:52 +0300

    #623 When in non-edit mode, booleans are shown as strings;

--- Modules/BetterCms.Module.Root/Mvc/Grids/EditableGridBooleanColumn.cs
+++ Modules/BetterCms.Module.Root/Mvc/Grids/EditableGridBooleanColumn.cs
@@ -2,6 +2,8 @@
 {
     public class EditableGridBooleanColumn : EditableGridColumn
     {
+        public bool ShowValueAsText { get; set; }
+
         public EditableGridBooleanColumn(string headerTitle = null, string sortColumn = null, string valueBind = null)
             : base(headerTitle, sortColumn, valueBind)
         {
