763429dd376b1578c5eb43ea917958dfbc8e6c7e
Audrunas Matonis <audrunas@devbridge.com>
2013-09-06 14:03:12 +0300

    #627 File / image preview boxes are rendered in the view instead of code helper; fixed position of messages

--- Modules/BetterCms.Module.MediaManager/Command/History/GetMediaVersion/GetMediaVersionCommand.cs
+++ Modules/BetterCms.Module.MediaManager/Command/History/GetMediaVersion/GetMediaVersionCommand.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 
 using BetterCms.Core.Mvc.Commands;
+
+using BetterCms.Module.MediaManager.Content.Resources;
 using BetterCms.Module.MediaManager.Models;
 using BetterCms.Module.MediaManager.Models.Extensions;
 using BetterCms.Module.MediaManager.ViewModels.History;
@@ -20,8 +22,41 @@ namespace BetterCms.Module.MediaManager.Command.History.GetMediaVersion
         /// <returns>Media preview.</returns>
         public MediaPreviewViewModel Execute(Guid request)
         {
-            var media = Repository.First<Media>(m => m.Id == request);
-            return new MediaPreviewViewModel { HtmlForPreview = media.GetPreviewHtml() };
+            var response = new MediaPreviewViewModel();
+
+            var media = Repository
+                .First<Media>(m => m.Id == request);
+
+
+            var image = media as MediaImage;
+            if (image != null)
+            {
+                response.AddProperty(MediaGlobalization.MediaHistory_Preview_Properties_Caption, image.Caption);
+                response.AddProperty(MediaGlobalization.MediaHistory_Preview_Properties_Title, image.Title);
+                response.AddProperty(MediaGlobalization.MediaHistory_Preview_Properties_Description, image.Description);
+                response.AddProperty(MediaGlobalization.MediaHistory_Preview_Properties_FileSize, image.SizeAsText());
+                response.AddProperty(MediaGlobalization.MediaHistory_Preview_Properties_ImageDimensions, string.Format("{0} x {1}", image.Width, image.Height));
+                response.AddProperty(MediaGlobalization.MediaHistory_Preview_Properties_PublicUrl, image.PublicUrl, true);
+                response.AddProperty(MediaGlobalization.MediaHistory_Preview_Properties_PublicThumbnailUrl, image.PublicThumbnailUrl, true);
+
+                response.AddProperty(image.Caption, image.PublicUrl, isImageUrl: true);
+            }
+
+            var file = media as MediaFile;
+            if (file != null)
+            {
+                response.AddProperty(MediaGlobalization.MediaHistory_Preview_Properties_Title, file.Title);
+                response.AddProperty(MediaGlobalization.MediaHistory_Preview_Properties_Description, file.Description);
+                response.AddProperty(MediaGlobalization.MediaHistory_Preview_Properties_FileSize, file.SizeAsText());
+                response.AddProperty(MediaGlobalization.MediaHistory_Preview_Properties_PublicUrl, file.PublicUrl, true);
+
+                if (media.Image != null)
+                {
+                    response.AddProperty(media.Image.Caption, media.Image.PublicUrl, isImageUrl: true);
+                }
+            }
+
+            return response;
         }
     }
 }
---
+++ Modules/BetterCms.Module.MediaManager/ViewModels/History/MediaPreviewPropertyViewModel.cs
@@ -0,0 +1,40 @@
+﻿namespace BetterCms.Module.MediaManager.ViewModels.History
+{
+    /// <summary>
+    /// View model for rendering media version properties
+    /// </summary>
+    public class MediaPreviewPropertyViewModel
+    {
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        /// <value>
+        /// The title.
+        /// </value>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether property is URL.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if property is URL; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether property is image URL.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if property is image URL; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsImageUrl { get; set; }
+    }
+}
