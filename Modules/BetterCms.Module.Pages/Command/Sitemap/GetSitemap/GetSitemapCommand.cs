900f08b0ca4787671f70de4331f76140b3f6ab68
paulius-maciulis <paulius@devbrige.com>
2013-09-11 16:55:47 +0300

    #678 sitemap second level nodes loading fixed.

--- Modules/BetterCms.Module.Pages/Command/Sitemap/GetSitemap/GetSitemapCommand.cs
+++ Modules/BetterCms.Module.Pages/Command/Sitemap/GetSitemap/GetSitemapCommand.cs
@@ -56,7 +56,7 @@ namespace BetterCms.Module.Pages.Command.Sitemap.GetSitemap
                         Title = node.Title,
                         Url = node.Url,
                         DisplayOrder = node.DisplayOrder,
-                        ChildNodes = GetSitemapNodesInHierarchy(sitemapNodes.Where(f => f.ParentNode == node).ToList())
+                        ChildNodes = GetSitemapNodesInHierarchy(node.ChildNodes)
                     });
             }
 
