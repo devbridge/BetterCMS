173408e2b6aab0d136289d96b968242e507eb74a
Simonas Mikulenas <simonas@devbridge.com>
2013-09-25 14:52:13 +0300

    #608: custom roles/users added to autocomplete; 30sec caching added.

--- Modules/BetterCms.Module.Root/Commands/Authentication/SearchRoles/SearchRolesCommand.cs
+++ Modules/BetterCms.Module.Root/Commands/Authentication/SearchRoles/SearchRolesCommand.cs
@@ -6,6 +6,7 @@ using System.Web.Security;
 using BetterCms.Configuration;
 using BetterCms.Core.Mvc.Commands;
 using BetterCms.Core.Security;
+using BetterCms.Core.Services.Caching;
 using BetterCms.Module.Root.Models;
 using BetterCms.Module.Root.Mvc;
 using BetterCms.Module.Root.ViewModels.Security;
@@ -18,17 +19,29 @@ namespace BetterCms.Module.Root.Commands.Authentication.SearchRoles
     public class SearchRolesCommand : CommandBase, ICommand<string, List<LookupKeyValue>>
     {
         /// <summary>
+        /// The cache key.
+        /// </summary>
+        private const string CacheKey = "bcms-rolesforautocomplete";
+
+        /// <summary>
         /// The configuration.
         /// </summary>
         private readonly ICmsConfiguration configuration;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="SearchRolesCommand"/> class.
+        /// The cache service.
+        /// </summary>
+        private readonly ICacheService cacheService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SearchRolesCommand" /> class.
         /// </summary>
         /// <param name="configuration">The configuration.</param>
-        public SearchRolesCommand(ICmsConfiguration configuration)
+        /// <param name="cacheService">The cache service.</param>
+        public SearchRolesCommand(ICmsConfiguration configuration, ICacheService cacheService)
         {
             this.configuration = configuration;
+            this.cacheService = cacheService;
         }
 
         /// <summary>
@@ -40,11 +53,20 @@ namespace BetterCms.Module.Root.Commands.Authentication.SearchRoles
         /// </returns>
         public List<LookupKeyValue> Execute(string roleName)
         {
-            if (!Roles.Enabled)
-            {
-                return null;
-            }
+            var allRoleNames = cacheService.Get(CacheKey, TimeSpan.FromSeconds(30), GetAllRoleNames);
 
+            return allRoleNames
+                .Where(role => role.ToLower().Contains(roleName.ToLower()))
+                .Select(role => new LookupKeyValue { Key = role, Value = role })
+                .ToList();
+        }
+
+        /// <summary>
+        /// Gets all role names.
+        /// </summary>
+        /// <returns>Distinct ordered role list.</returns>
+        private IList<string> GetAllRoleNames()
+        {
             IList<IAccessRule> list = new List<IAccessRule>();
             foreach (AccessControlElement userAccess in configuration.Security.DefaultAccessRules)
             {
@@ -58,17 +80,28 @@ namespace BetterCms.Module.Root.Commands.Authentication.SearchRoles
             }
 
             var result = list
-                .Where(accessRule => accessRule.IsForRole && accessRule.Identity.ToLower().Contains(roleName.ToLower()))
+                .Where(accessRule => accessRule.IsForRole)
                 .Select(accessRule => accessRule.Identity)
+                .Distinct()
                 .ToList();
 
-            result.AddRange(Roles.Provider.GetAllRoles()
-                .Where(role => role.ToLower().Contains(roleName.ToLower()))
-                .ToList());
+            if (Roles.Enabled)
+            {
+                result.AddRange(Roles.Provider.GetAllRoles()
+                    .Distinct()
+                    .ToList());
+            }
+
+            result.AddRange(
+                Repository.AsQueryable<AccessRule>()
+                    .Where(a => a.IsForRole)
+                    .Select(a => a.Identity)
+                    .Distinct()
+                    .ToList());
 
             return result
+                .Distinct()
                 .OrderBy(role => role)
-                .Select(role => new LookupKeyValue { Key = role, Value = role })
                 .ToList();
         }
     }
--- Modules/BetterCms.Module.Root/Commands/Authentication/SearchUsers/SearchUsersCommand.cs
+++ Modules/BetterCms.Module.Root/Commands/Authentication/SearchUsers/SearchUsersCommand.cs
@@ -6,9 +6,12 @@ using System.Web.Security;
 using BetterCms.Configuration;
 using BetterCms.Core.Mvc.Commands;
 using BetterCms.Core.Security;
+using BetterCms.Core.Services.Caching;
 using BetterCms.Module.Root.Models;
 using BetterCms.Module.Root.Mvc;
 using BetterCms.Module.Root.ViewModels.Security;
+
+using Common.Logging;
 
 namespace BetterCms.Module.Root.Commands.Authentication.SearchUsers
 {
@@ -18,17 +21,34 @@ namespace BetterCms.Module.Root.Commands.Authentication.SearchUsers
     public class SearchUsersCommand : CommandBase, ICommand<string, List<LookupKeyValue>>
     {
         /// <summary>
+        /// The cache key.
+        /// </summary>
+        private const string CacheKey = "bcms-usersforautocomplete";
+
+        /// <summary>
+        /// Current class logger.
+        /// </summary>
+        private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
         /// The configuration.
         /// </summary>
         private readonly ICmsConfiguration configuration;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="SearchUsersCommand"/> class.
+        /// The cache service.
+        /// </summary>
+        private readonly ICacheService cacheService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SearchUsersCommand" /> class.
         /// </summary>
         /// <param name="configuration">The configuration.</param>
-        public SearchUsersCommand(ICmsConfiguration configuration)
+        /// <param name="cacheService">The cache service.</param>
+        public SearchUsersCommand(ICmsConfiguration configuration, ICacheService cacheService)
         {
             this.configuration = configuration;
+            this.cacheService = cacheService;
         }
 
         /// <summary>
@@ -39,6 +59,20 @@ namespace BetterCms.Module.Root.Commands.Authentication.SearchUsers
         /// A list of users.
         /// </returns>
         public List<LookupKeyValue> Execute(string userName)
+        {
+            var allUserNames = cacheService.Get(CacheKey, TimeSpan.FromSeconds(30), GetAllUserNames);
+
+            return allUserNames
+                .Where(user => user.ToLower().Contains(userName.ToLower()))
+                .Select(user => new LookupKeyValue { Key = user, Value = user })
+                .ToList();
+        }
+
+        /// <summary>
+        /// Gets all user names.
+        /// </summary>
+        /// <returns>Distinct ordered username list.</returns>
+        private IList<string> GetAllUserNames()
         {
             IList<IAccessRule> list = new List<IAccessRule>();
             foreach (AccessControlElement userAccess in configuration.Security.DefaultAccessRules)
@@ -53,19 +87,35 @@ namespace BetterCms.Module.Root.Commands.Authentication.SearchUsers
             }
 
             var result = list
-                .Where(accessRule => !accessRule.IsForRole && accessRule.Identity.ToLower().Contains(userName.ToLower()))
+                .Where(accessRule => !accessRule.IsForRole)
                 .Select(accessRule => accessRule.Identity)
+                .Distinct()
                 .ToList();
 
-            result.AddRange(Membership.GetAllUsers()
-                .Cast<MembershipUser>()
-                .Where(a => a.UserName.ToLower().Contains(userName.ToLower()))
-                .Select(a => a.UserName)
-                .ToList());
+            try
+            {
+                result.AddRange(
+                    Membership.GetAllUsers()
+                        .Cast<MembershipUser>()
+                        .Select(a => a.UserName)
+                        .Distinct()
+                        .ToList());
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error occurred while retrieving users.", ex);
+            }
+
+            result.AddRange(
+                Repository.AsQueryable<AccessRule>()
+                    .Where(a => !a.IsForRole)
+                    .Select(a => a.Identity)
+                    .Distinct()
+                    .ToList());
 
             return result
+                .Distinct()
                 .OrderBy(user => user)
-                .Select(user => new LookupKeyValue { Key = user, Value = user })
                 .ToList();
         }
     }
