345e409d2e8152f0894fcf0f3e5303120441e839
Simonas Mikulenas <simonas@devbridge.com>
2013-10-28 12:28:28 +0200

    #740: access right checking added to preview.

--- Modules/BetterCms.Module.Root/Commands/GetPageToRender/GetPageToRenderCommand.cs
+++ Modules/BetterCms.Module.Root/Commands/GetPageToRender/GetPageToRenderCommand.cs
@@ -77,7 +77,7 @@ namespace BetterCms.Module.Root.Commands.GetPageToRender
 
             // Preview and published pages can be accessible to users without content managing roles
             // Unpublished pages can be accessible only to content managers
-            if (page.Status != PageStatus.Published && page.Status != PageStatus.Preview && !request.CanManageContent)
+            if (page.Status != PageStatus.Published && page.Status != PageStatus.Preview && !request.HasContentAccess)
             {
                 if (!cmsConfiguration.Security.AccessControlEnabled)
                 {
--- Modules/BetterCms.Module.Root/Controllers/PreviewController.cs
+++ Modules/BetterCms.Module.Root/Controllers/PreviewController.cs
@@ -1,7 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 
 using BetterCms.Core.Mvc.Attributes;
 using BetterCms.Core.Security;
+using BetterCms.Core.Services.Caching;
 using BetterCms.Module.Root.Commands.GetPageToRender;
 using BetterCms.Module.Root.Mvc;
 
@@ -17,6 +19,20 @@ namespace BetterCms.Module.Root.Controllers
     public class PreviewController : CmsControllerBase
     {
         /// <summary>
+        /// The CMS configuration.
+        /// </summary>
+        private readonly ICmsConfiguration cmsConfiguration;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PreviewController"/> class.
+        /// </summary>
+        /// <param name="cmsConfiguration">The CMS configuration.</param>
+        public PreviewController(ICmsConfiguration cmsConfiguration)
+        {
+            this.cmsConfiguration = cmsConfiguration;
+        }
+
+        /// <summary>
         /// Previews the specified page id.
         /// </summary>
         /// <param name="pageId">The page id.</param>
@@ -27,11 +43,20 @@ namespace BetterCms.Module.Root.Controllers
         [IgnoreAutoRoute]
         public ActionResult Index(string pageId, string pageContentId)
         {
+            var principal = SecurityService.GetCurrentPrincipal();
+
+            var allRoles = new List<string>(RootModuleConstants.UserRoles.AllRoles);
+            if (!string.IsNullOrEmpty(cmsConfiguration.Security.FullAccessRoles))
+            {
+                allRoles.Add(cmsConfiguration.Security.FullAccessRoles);
+            }
+
             GetPageToRenderRequest request = new GetPageToRenderRequest
                 {
                     PageId = pageId.ToGuidOrDefault(),
                     PreviewPageContentId = pageContentId.ToGuidOrDefault(),
-                    IsPreview = true
+                    IsPreview = true,
+                    HasContentAccess = SecurityService.IsAuthorized(principal, RootModuleConstants.UserRoles.MultipleRoles(allRoles.ToArray()))
                 };
 
             var model = GetCommand<GetPageToRenderCommand>().ExecuteCommand(request);
