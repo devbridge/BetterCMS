7c5b02f5906c28f748aca473944b034f626bd93f
Simonas Mikulenas <simonas@devbridge.com>
2013-11-19 14:43:23 +0200

    #769: blank template added.

---
+++ Modules/BetterCms.Module.Installation/Models/Migrations/Migration201311181011.sqlserver.sql
@@ -0,0 +1,67 @@
+﻿INSERT INTO [bcms_root].[Layouts]
+           ([Id]
+           ,[Version]
+           ,[IsDeleted]
+           ,[CreatedOn]
+           ,[CreatedByUser]
+           ,[ModifiedOn]
+           ,[ModifiedByUser]
+           ,[Name]
+           ,[LayoutPath])
+     VALUES
+           ('24A9FDBC-7A9B-4C02-AFF5-A0B09F2CA9E7'
+           ,1
+           ,0
+           ,getdate()
+           ,'Better CMS'
+           ,getdate()
+           ,'Better CMS'
+           ,'Blank Template'
+           ,'~/Areas/bcms-installation/Views/Shared/BlankLayout.cshtml')
+
+IF NOT EXISTS (
+	SELECT 1 
+	FROM [bcms_root].[Regions]
+	WHERE [RegionIdentifier] = 'CMSMainContent' AND IsDeleted = 0
+)
+INSERT INTO [bcms_root].[Regions]
+           ([Id]
+           ,[Version]
+           ,[IsDeleted]
+           ,[CreatedOn]
+           ,[CreatedByUser]
+           ,[ModifiedOn]
+           ,[ModifiedByUser]
+           ,[RegionIdentifier])
+     VALUES
+           ('CA3216C0-3E27-4785-BB55-245963177CC6'
+           ,1
+           ,0
+           ,getdate()
+           ,'Better CMS'
+           ,getdate()
+           ,'Better CMS'
+           ,'CMSMainContent')
+
+INSERT INTO [bcms_root].[LayoutRegions]
+           ([Id]
+           ,[Version]
+           ,[IsDeleted]
+           ,[CreatedOn]
+           ,[CreatedByUser]
+           ,[ModifiedOn]
+           ,[ModifiedByUser]
+           ,[LayoutId]
+           ,[RegionId])
+     VALUES
+           ('26CE115F-2BE0-4B11-8495-AF3E0A9FBDAF'
+           ,1
+           ,0
+           ,getdate()
+           ,'Better CMS'
+           ,getdate()
+           ,'Better CMS'
+           ,'24A9FDBC-7A9B-4C02-AFF5-A0B09F2CA9E7'
+           ,(SELECT top 1 Id 
+               FROM [bcms_root].[Regions] 
+              WHERE ([RegionIdentifier] = 'CMSMainContent' AND IsDeleted = 0)))
