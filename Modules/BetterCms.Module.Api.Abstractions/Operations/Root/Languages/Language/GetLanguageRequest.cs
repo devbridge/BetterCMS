3f0219e814be3996acbf16f0ed56417f1e57c595
Audrunas Matonis <audrunas@devbridge.com>
2013-12-31 10:39:11 +0200

    #842 Created API methods and tests for getting the list of languages

---
+++ Modules/BetterCms.Module.Api.Abstractions/Operations/Root/Languages/Language/GetLanguageRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+using ServiceStack.ServiceHost;
+
+namespace BetterCms.Module.Api.Operations.Root.Languages.Language
+{
+    [Route("/languages/{LanguageId}", Verbs = "GET")]
+    [Route("/languages/by-code/{LanguageCode}", Verbs = "GET")]
+    [DataContract]
+    public class GetLanguageRequest : IReturn<GetLanguageResponse>
+    {
+        [DataMember]
+        public Guid? LanguageId { get; set; }
+
+        [DataMember]
+        public string LanguageCode { get; set; }
+    }
+}
---
+++ Modules/BetterCms.Module.Api/Operations/Root/Languages/Language/LanguageService.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+
+using BetterCms.Core.DataAccess;
+using BetterCms.Core.DataAccess.DataContext;
+
+using ServiceStack.ServiceInterface;
+
+namespace BetterCms.Module.Api.Operations.Root.Languages.Language
+{
+    public class LanguageService : Service, ILanguageService
+    {
+        private readonly IRepository repository;
+
+        public LanguageService(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public GetLanguageResponse Get(GetLanguageRequest request)
+        {
+            var query = repository.AsQueryable<Module.Root.Models.Language>();
+
+            if (request.LanguageId.HasValue)
+            {
+                query = query.Where(language => language.Id == request.LanguageId);
+            }
+            else
+            {
+                query = query.Where(language => language.Code == request.LanguageCode);
+            }
+
+            var model = query
+                .Select(language => new LanguageModel
+                    {
+                        Id = language.Id,
+                        Version = language.Version,
+                        CreatedBy = language.CreatedByUser,
+                        CreatedOn = language.CreatedOn,
+                        LastModifiedBy = language.ModifiedByUser,
+                        LastModifiedOn = language.ModifiedOn,
+
+                        Name = language.Name,
+                        Code = language.Code
+                    })
+                .FirstOne();
+
+            return new GetLanguageResponse
+                       {
+                           Data = model
+                       };
+        }
+    }
+}
