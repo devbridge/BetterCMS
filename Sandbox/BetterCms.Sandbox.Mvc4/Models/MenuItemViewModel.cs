5f0041b82620652f0ced71fd83dcd2212036ca2c
Simonas Mikulenas <simonas@devbridge.com>
2014-02-05 07:54:48 +0200

    #952: test menu widget added to the sandbox.

---
+++ Sandbox/BetterCms.Sandbox.Mvc4/Models/MenuItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BetterCms.Sandbox.Mvc4.Models
+{
+    public class MenuItemViewModel
+    {
+        public string Caption { get; set; }
+
+        public string Url { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Caption: {0}, Url: {1}", Caption, Url);
+        }
+    }
+}
