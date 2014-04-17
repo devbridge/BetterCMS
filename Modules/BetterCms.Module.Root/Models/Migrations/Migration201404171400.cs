7d955756f1991cc516e5ee35823e0580565c1c3b
Audrunas Matonis <audrunas@devbridge.com>
2014-04-17 13:52:06 +0300

    #1017 Created sql fix for master pages with status == unpublished

---
+++ Modules/BetterCms.Module.Root/Models/Migrations/Migration201404171400.cs
@@ -0,0 +1,30 @@
+﻿using BetterCms.Core.DataAccess.DataContext.Migrations;
+using BetterCms.Core.DataContracts.Enums;
+
+using FluentMigrator;
+
+namespace BetterCms.Module.Root.Models.Migrations
+{
+    /// <summary>
+    /// Fix for issue: https://github.com/devbridge/BetterCMS/issues/840
+    /// </summary>
+    [Migration(201404171400)]
+    public class Migration201404171400 : DefaultMigration
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Migration201404171400"/> class.
+        /// </summary>
+        public Migration201404171400()
+            : base(RootModuleDescriptor.ModuleName)
+        {
+        }
+
+        public override void Up()
+        {
+            Update.Table("Pages")
+                .InSchema(SchemaName)
+                .Set(new { Status = (int)PageStatus.Published })
+                .Where(new { Status = (int)PageStatus.Unpublished, IsDeleted = 0, IsMasterPage = 1 });
+        }       
+    }
+}
