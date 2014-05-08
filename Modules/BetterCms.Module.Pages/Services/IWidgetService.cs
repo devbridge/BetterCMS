a0dcda46f81c39bdfa15be01183eb6be802c37e1
Audrunas Matonis <audrunas@devbridge.com>
2014-05-08 14:48:41 +0300

    #1032 API: created PUT method for server control widgets

--- Modules/BetterCms.Module.Pages/Services/IWidgetService.cs
+++ Modules/BetterCms.Module.Pages/Services/IWidgetService.cs
@@ -6,5 +6,7 @@ namespace BetterCms.Module.Pages.Services
     public interface IWidgetService
     {
         void SaveHtmlContentWidget(EditHtmlContentWidgetViewModel model, out HtmlContentWidget widget, out HtmlContentWidget originalWidget);
+
+        ServerControlWidget SaveServerControlWidget(EditServerControlWidgetViewModel model);
     }
 }
