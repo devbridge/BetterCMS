d5c9edf80bdf94e969a1348fe9346eb52b2cfb86
Audrunas Matonis <audrunas@devbridge.com>
2014-05-21 11:58:20 +0300

    #1026 API: Created tests for authors

---
+++ Tests/BetterCms.Modules.Tests/Api/Blog/Authors/AuthorsApiTests.cs
@@ -0,0 +1,60 @@
+﻿using BetterCms.Core.Models;
+using BetterCms.Module.Api.Extensions;
+using BetterCms.Module.Api.Operations.Blog.Authors;
+using BetterCms.Module.Api.Operations.Blog.Authors.Author;
+
+using NHibernate;
+
+using NUnit.Framework;
+
+namespace BetterCms.Test.Module.Api.Blog.Authors
+{
+    public class AuthorsApiTests : ApiCrudIntegrationTestBase<
+        SaveAuthorModel, AuthorModel,
+        PostAuthorRequest, PostAuthorResponse,
+        GetAuthorRequest, GetAuthorResponse,
+        PutAuthorRequest, PutAuthorResponse,
+        DeleteAuthorRequest, DeleteAuthorResponse>
+    {
+        [Test]
+        public void Should_CRUD_Author_Successfully()
+        {
+            RunApiActionInTransaction((api, session) =>
+                Run(session, api.Blog.Authors.Post, api.Blog.Author.Get, api.Blog.Author.Put, api.Blog.Author.Delete));
+        }
+
+        protected override SaveAuthorModel GetCreateModel(ISession session)
+        {
+            var image = TestDataProvider.CreateNewMediaImage();
+            session.SaveOrUpdate(image);
+
+            return new SaveAuthorModel
+                {
+                    Name = TestDataProvider.ProvideRandomString(MaxLength.Name),
+                    ImageId = image.Id
+                };
+        }
+
+        protected override GetAuthorRequest GetGetRequest(BetterCms.Module.Api.Infrastructure.SaveResponseBase saveResponseBase)
+        {
+            return new GetAuthorRequest { AuthorId = saveResponseBase.Data.Value };
+        }
+
+        protected override PutAuthorRequest GetUpdateRequest(GetAuthorResponse getResponse)
+        {
+            var request = getResponse.ToPutRequest();
+            request.Data.Name = TestDataProvider.ProvideRandomString(MaxLength.Name);
+
+            return request;
+        }
+
+        protected override void OnAfterGet(GetAuthorResponse getResponse, SaveAuthorModel model)
+        {
+            Assert.IsNotNull(getResponse.Data.Name);
+            Assert.IsNotNull(getResponse.Data.ImageId);
+
+            Assert.AreEqual(getResponse.Data.Name, model.Name);
+            Assert.AreEqual(getResponse.Data.ImageId, model.ImageId);
+        }
+    }
+}
