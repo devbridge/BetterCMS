c0d74d1d67c25f70e0d6176e22704bd8662faffa
Audrunas Matonis <audrunas@devbridge.com>
2014-06-10 16:07:59 +0300

    #1067 Page view SQL migration script fix

--- Modules/BetterCms.Module.Pages/Models/Migrations/Migration201406101545.sqlserver.sql
+++ Modules/BetterCms.Module.Pages/Models/Migrations/Migration201406101545.sqlserver.sql
@@ -22,31 +22,30 @@ AS
 							UNION
 
 							SELECT 1
-                              FROM [BetterCms].[bcms_pages].[SitemapNodes] AS n1
-					    INNER JOIN [BetterCms].[bcms_pages].[Sitemaps] AS s1 ON s1.Id = n1.SitemapId
+                              FROM [bcms_pages].[SitemapNodes] AS n1
+					    INNER JOIN [bcms_pages].[Sitemaps] AS s1 ON s1.Id = n1.SitemapId
 							 WHERE (n1.PageId = p.Id OR n1.UrlHash = p.[PageUrlHash]) AND n1.IsDeleted = 0 AND s1.IsDeleted = 0
 
 							UNION
 
 							SELECT 1
-                              FROM [BetterCms].[bcms_pages].[SitemapNodeTranslations] AS t
-					    INNER JOIN [BetterCms].[bcms_pages].[SitemapNodes] AS n3 ON n3.Id = t.NodeId
-					    INNER JOIN [BetterCms].[bcms_pages].[Sitemaps] AS s3 ON s3.Id = n3.SitemapId
+                              FROM [bcms_pages].[SitemapNodeTranslations] AS t
+					    INNER JOIN [bcms_pages].[SitemapNodes] AS n3 ON n3.Id = t.NodeId
+					    INNER JOIN [bcms_pages].[Sitemaps] AS s3 ON s3.Id = n3.SitemapId
 							 WHERE t.UrlHash = p.[PageUrlHash] AND t.IsDeleted = 0  AND n3.IsDeleted = 0 AND s3.IsDeleted = 0
 
 							UNION
 
 							SELECT 1
-                              FROM [BetterCms].[bcms_pages].[SitemapNodes] AS n1
-					    INNER JOIN [BetterCms].[bcms_pages].[Sitemaps] AS s1 ON s1.Id = n1.SitemapId
+                              FROM [bcms_pages].[SitemapNodes] AS n1
+					    INNER JOIN [bcms_pages].[Sitemaps] AS s1 ON s1.Id = n1.SitemapId
 							 WHERE 
 								   EXISTS (SELECT gp.Id
-							                 FROM [BetterCms].[bcms_root].[Pages] as gp
+							                 FROM [bcms_root].[Pages] as gp
 								            WHERE gp.LanguageGroupIdentifier = p.LanguageGroupIdentifier AND n1.PageId = gp.Id)
 								   AND n1.IsDeleted = 0 AND s1.IsDeleted = 0
 							) as x) = 0 then 0 else 1 end) AS BIT)
 
-       FROM [BetterCms].[bcms_root].[Pages] AS p
+       FROM [bcms_root].[Pages] AS p
        WHERE p.IsDeleted = 0
-	   ORDER BY PageUrl
 GO
