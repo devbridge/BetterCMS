50f5218ff9b63f02b5b398792a9ec93316f7628f
Simonas Mikulenas <simonas@devbridge.com>
2014-06-23 14:46:55 +0300

    API sitemap test updated.

--- Tests/BetterCms.Modules.Tests/Api/Pages/Sitemaps/SitemapsTests.cs
+++ Tests/BetterCms.Modules.Tests/Api/Pages/Sitemaps/SitemapsTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 using BetterCms.Core.Models;
@@ -23,6 +24,7 @@ namespace BetterCms.Test.Module.Api.Pages.Sitemaps
         DeleteSitemapRequest, DeleteSitemapResponse>
     {
         private int createdNodeEventCount;
+        private int deletedNodeEventCount;
 
         [Test]
         public void Should_CRUD_Sitemap_Successfully()
@@ -32,21 +34,29 @@ namespace BetterCms.Test.Module.Api.Pages.Sitemaps
             Events.SitemapEvents.Instance.SitemapUpdated += Instance_EntityUpdated;
             Events.SitemapEvents.Instance.SitemapDeleted += Instance_EntityDeleted;
             Events.SitemapEvents.Instance.SitemapNodeCreated += Instance_SitemapNodeCreated;
+            Events.SitemapEvents.Instance.SitemapNodeDeleted += Instance_SitemapNodeDeleted;
 
             RunApiActionInTransaction((api, session) => Run(session, api.Pages.Sitemaps.Post, api.Pages.SitemapNew.Get, api.Pages.SitemapNew.Put, api.Pages.SitemapNew.Delete));
 
             Assert.AreEqual(2, createdNodeEventCount, "Created node events fired count");
+            Assert.AreEqual(1, deletedNodeEventCount, "Deleted node events fired count");
 
             // Detach from events
             Events.SitemapEvents.Instance.SitemapCreated -= Instance_EntityCreated;
             Events.SitemapEvents.Instance.SitemapUpdated -= Instance_EntityUpdated;
             Events.SitemapEvents.Instance.SitemapDeleted -= Instance_EntityDeleted;
             Events.SitemapEvents.Instance.SitemapNodeCreated -= Instance_SitemapNodeCreated;
+            Events.SitemapEvents.Instance.SitemapNodeDeleted += Instance_SitemapNodeDeleted;
         }
 
         void Instance_SitemapNodeCreated(Events.SingleItemEventArgs<BetterCms.Module.Pages.Models.SitemapNode> args)
         {
             createdNodeEventCount++;
+        }
+
+        void Instance_SitemapNodeDeleted(Events.SingleItemEventArgs<BetterCms.Module.Pages.Models.SitemapNode> args)
+        {
+            deletedNodeEventCount++;
         }
 
         protected override SaveSitemapModel GetCreateModel(ISession session)
@@ -66,7 +76,18 @@ namespace BetterCms.Test.Module.Api.Pages.Sitemaps
                                                PageId = null,
                                                Macro = TestDataProvider.ProvideRandomString(MaxLength.Text),
                                                UsePageTitleAsNodeTitle = false,
-                                               Translations = new List<SaveSitemapNodeTranslation>(),
+                                               Translations = new List<SaveSitemapNodeTranslation>
+                                                                  {
+                                                                      new SaveSitemapNodeTranslation
+                                                                          {
+                                                                              Id = Guid.NewGuid(),
+                                                                              Title = "Some translation title",
+                                                                              Url = TestDataProvider.ProvideRandomString(MaxLength.Url),
+                                                                              UsePageTitleAsNodeTitle = false,
+                                                                              LanguageId = Guid.Empty,
+                                                                              Macro = "test"
+                                                                          }
+                                                                  },
                                                Nodes =
                                                    new[]
                                                        {
@@ -96,6 +117,10 @@ namespace BetterCms.Test.Module.Api.Pages.Sitemaps
         {
             var request = getResponse.ToPutRequest();
             request.Data.Title = this.TestDataProvider.ProvideRandomString(MaxLength.Name);
+            request.Data.Nodes.First().Translations.First().Url = TestDataProvider.ProvideRandomString(MaxLength.Url);
+            request.Data.Nodes.First().Nodes.Clear();
+            request.Data.AccessRules.Clear();
+
             return request;
         }
 
@@ -106,13 +131,12 @@ namespace BetterCms.Test.Module.Api.Pages.Sitemaps
             Assert.AreEqual(getResponse.Data.Title, saveModel.Title);
             Assert.AreEqual(getResponse.Data.Tags.Count, saveModel.Tags.Count);
 
-            Assert.AreEqual(getResponse.Nodes.Count, 2);
             Assert.AreEqual(getResponse.Nodes.First(n => n.ParentId == null).Title, saveModel.Nodes.First().Title);
+            Assert.AreEqual(getResponse.Nodes.First(n => n.ParentId == null).Translations.First().Title, saveModel.Nodes.First().Translations.First().Title);
 
-            Assert.AreEqual(getResponse.AccessRules.Count, 1);
-            Assert.AreEqual(getResponse.AccessRules[0].AccessLevel, saveModel.AccessRules[0].AccessLevel);
-            Assert.AreEqual(getResponse.AccessRules[0].Identity, saveModel.AccessRules[0].Identity);
-            Assert.AreEqual(getResponse.AccessRules[0].IsForRole, saveModel.AccessRules[0].IsForRole);
+//            Assert.AreEqual(getResponse.AccessRules[0].AccessLevel, saveModel.AccessRules[0].AccessLevel);
+//            Assert.AreEqual(getResponse.AccessRules[0].Identity, saveModel.AccessRules[0].Identity);
+//            Assert.AreEqual(getResponse.AccessRules[0].IsForRole, saveModel.AccessRules[0].IsForRole);
         }
     }
 }
