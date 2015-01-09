7bb19310ba2407f2131808fcf65985607d2324c2
Simonas Mikulenas <simonas@devbridge.com>
2015-01-09 18:53:53 +0200

    #1187: shim configuration providing added.

---
+++ BetterCms.Core/Modules/JsShimConfigDescriptor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace BetterCms.Core.Modules
+{
+    public class JsShimConfigDescriptor
+    {
+        public string Exports { get; set; }
+
+        public List<string> Depends { get; set; }
+    }
+}
---
+++ Modules/BetterCms.Module.Root/ViewModels/Rendering/JavaScriptModuleShimConfigurationViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace BetterCms.Module.Root.ViewModels.Rendering
+{
+    public class JavaScriptModuleShimConfigurationViewModel
+    {
+        public string Exports { get; set; }
+
+        public List<string> Depends { get; set; }
+    }
+}
