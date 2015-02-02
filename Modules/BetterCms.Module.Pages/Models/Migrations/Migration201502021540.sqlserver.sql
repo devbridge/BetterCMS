3b5b8cec748018e0e7721318d79684faa2be2d13
Rimvydas Urbonas <rimvydas.urbonas@devbridge.com>
2015-02-02 15:51:39 +0200

    Categories data migration scripts.

---
+++ Modules/BetterCms.Module.Pages/Models/Migrations/Migration201502021540.sqlserver.sql
@@ -0,0 +1,8 @@
+﻿ BEGIN
+    MERGE bcms_Pages.PageCategories AS target
+    USING ( select Id, CategoryId from bcms_pages.Pages where CategoryId is not null) AS source (PageId, CategoryId)
+    ON ( target.PageId = source.PageId and target.CategoryId = source.CategoryId)
+	WHEN NOT MATCHED THEN
+    INSERT ( PageId, CategoryId)
+    VALUES (source. PageId, source.CategoryId);
+ END
