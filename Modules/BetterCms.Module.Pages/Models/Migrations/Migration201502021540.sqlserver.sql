b4960ee47318f7c16672691684d4f03c70401644
Rimvydas Urbonas <rimvydas.urbonas@devbridge.com>
2015-02-03 09:12:54 +0200

    Upgrade test mdf with changes.

--- Modules/BetterCms.Module.Pages/Models/Migrations/Migration201502021540.sqlserver.sql
+++ Modules/BetterCms.Module.Pages/Models/Migrations/Migration201502021540.sqlserver.sql
@@ -3,6 +3,6 @@
     USING ( select Id, CategoryId from bcms_pages.Pages where CategoryId is not null) AS source (PageId, CategoryId)
     ON ( target.PageId = source.PageId and target.CategoryId = source.CategoryId)
 	WHEN NOT MATCHED THEN
-    INSERT ( PageId, CategoryId)
-    VALUES (source. PageId, source.CategoryId);
+    INSERT ( PageId, CategoryId, Version, CreatedByUser, CreatedOn, ModifiedByUser, ModifiedOn)
+    VALUES (source. PageId, source.CategoryId, 0, 'Admin', getDate(), 'Admin', getDate());
  END
