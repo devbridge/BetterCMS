6dd37877dccc5bf054962e40d038fdc4f84d7e98
Simonas Mikulenas <simonas@devbridge.com>
2015-02-07 16:36:29 +0200

    #1189: API test for categories updated.

--- Tests/BetterCms.Api.Tests/spec/root.categories.api.behavior.js
+++ Tests/BetterCms.Api.Tests/spec/root.categories.api.behavior.js
@@ -4,25 +4,27 @@
 describe('root.categories.api.behavior', function () {
     'use strict';
 
-    it('00300: Should get categories list', function () {
-        var url = '/bcms-api/categories/',
+    var constants = {
+        defaultCategoryTreeId: '98fd87b4a25c4dde933c83826b6a94d7',
+
+        categoryNodeId: 'e87bfb18cdf74fd3a5dfa2040115ed1d',
+        categoryNodeName: '_0001_ - 3',
+    };
+    
+    it('00300: Should get a list of category trees.', function () {
+        var url = '/bcms-api/categorytrees/',
             result,
-            ready = false;
-        
-        var data = {
+            ready = false,
+            data = {
                 filter: {
-                    where: [
-                        { field: 'Name', operation: 'StartsWith', value: '_0001_' }
-                    ]
+                    connector: 'or',
+                    where: []
                 },
                 order: {
-                    by: [
-                        { field: 'Name' }
-                    ]
+                    by: [{ field: 'Name', direction: 'desc' }]
                 },
-                skip: 2,
-                take: 3
-        };
+                take: 2
+            };
 
         runs(function () {
             api.get(url, data, function (json) {
@@ -38,17 +40,42 @@ describe('root.categories.api.behavior', function () {
         runs(function () {
             expect(result).toBeDefinedAndNotNull('JSON object should be retrieved.');
             expect(result.data).toBeDefinedAndNotNull('JSON data object should be retrieved.');
-            expect(result.data.totalCount).toBe(5, 'Total count should be 5.');
-            expect(result.data.items.length).toBe(3, 'Returned array length should be 3.');
+            expect(result.data.items).not.toBeNull('JSON data.items object should be retrieved.');
 
-            expect(result.data.items[0].name).toBe('_0001_ - 3', 'Correctly filtered items[0].name should be retrieved.');
-            expect(result.data.items[1].name).toBe('_0001_ - 4', 'Correctly filtered items[1].name should be retrieved.');
-            expect(result.data.items[2].name).toBe('_0001_ - 5', 'Correctly filtered items[2].name should be retrieved.');
+            expect(result.data.totalCount).toBe(1, 'Total count should be 1.');
+            expect(result.data.items.length).toBe(1, 'Returned array length should be 1.');
         });
     });
 
-    it('00301: Should get a category by id', function () {
-        var url = '/bcms-api/categories/e87bfb18cdf74fd3a5dfa2040115ed1d',
+    it('00301: Should get a category tree.', function () {
+        var url = '/bcms-api/categorytrees/' + constants.defaultCategoryTreeId,
+            result,
+            ready = false,
+            data = {
+                includeAccessRules: true,
+            };
+
+        runs(function () {
+            api.get(url, data, function (json) {
+                result = json;
+                ready = true;
+            });
+        });
+
+        waitsFor(function () {
+            return ready;
+        }, 'The ' + url + ' timeout.');
+
+        runs(function () {
+            expect(result).toBeDefinedAndNotNull('JSON object should be retrieved.');
+            expect(result.data).toBeDefinedAndNotNull('JSON data object should be retrieved.');
+            expect(result.data.title).not.toBeNull('JSON data.title object should be retrieved.');
+            expect(result.data.nodes).not.toBeNull('JSON data.tags object should be retrieved.');
+        });
+    });
+
+    it('00302: Should get the full category tree.', function () {
+        var url = '/bcms-api/categorytrees/' + constants.defaultCategoryTreeId + '/tree',
             result,
             ready = false;
 
@@ -66,29 +93,143 @@ describe('root.categories.api.behavior', function () {
         runs(function () {
             expect(result).toBeDefinedAndNotNull('JSON object should be retrieved.');
             expect(result.data).toBeDefinedAndNotNull('JSON data object should be retrieved.');
+            expect(result.data.length).toBeGreaterThan(0);
 
-            api.expectBasePropertiesAreNotNull(result.data);
-            expect(result.data.name).toBe('_0001_ - 3', 'Correctly filtered name should be retrieved.');
+            var rootFound = false;
+            for (var i = 0; i < result.data.length; i++) {
+                if (result.data[i].name == constants.categoryNodeName) {
+                    var rootNode = result.data[i];
+                    rootFound = true;
+
+                    api.expectBasePropertiesAreNotNull(rootNode);
+                    expect(rootNode.parentId).toBeNull('parentId should be null.');
+                    expect(rootNode.displayOrder).toBeDefinedAndNotNull('displayOrder should be retrieved.');
+                    expect(rootNode.childrenNodes).toBeDefinedAndNotNull('childrenNodes should be retrieved.');
+                    expect(rootNode.childrenNodes.length).toBe(0, 'Returned childrenNodes array length should be 0.');
+                }
+            }
+
+            expect(rootFound).toBe(true, 'Root node should be retrieved.');
         });
     });
-    
-    it('00302: Should get a list with one category, filtered by all available columns', function () {
-        var url = '/bcms-api/categories/',
+
+    it('00303: Should get category tree, filtered by node.', function () {
+        var url = '/bcms-api/categorytrees/' + constants.defaultCategoryTreeId + '/tree',
+            result,
+            ready = false,
+            data = {
+                nodeId: constants.categoryNodeId
+            };
+
+        runs(function () {
+            api.get(url, data, function (json) {
+                result = json;
+                ready = true;
+            });
+        });
+
+        waitsFor(function () {
+            return ready;
+        }, 'The ' + url + ' timeout.');
+
+        runs(function () {
+            expect(result).toBeDefinedAndNotNull('JSON object should be retrieved.');
+            expect(result.data).toBeDefinedAndNotNull('JSON data object should be retrieved.');
+            expect(result.data.length).toBe(0, 'Returned array length should be 0.');
+// TODO: update default category tree not to have one child node '_0001_ - 3'.
+//            var node = result.data[0];
+//            api.expectBasePropertiesAreNotNull(node);
+//            expect(node.name).toBe(constants.categoryNodeName, 'Correctly filtered title should be retrieved.');
+//            expect(node.parentId).toBe(constants.categoryNodeId, 'Correctly filtered parentId should be retrieved.');
+//            expect(node.childrenNodes).toBeDefinedAndNotNull('childrenNodes should be retrieved.');
+//            expect(node.childrenNodes.length).toBe(0, 'Correctly filtered count of children nodes should be retrieved.');
+        });
+    });
+
+    it('00304: Should test CRUD for category trees.', function () {
+        api.testCrud(runs, waitsFor, expect, constants.defaultCategoryTreeId, "/bcms-api/categorytrees/", {
+            getGetData: function () {
+                return {
+                    IncludeNodes: true,
+                    includeAccessRules: false
+                }
+            },
+            getPostData: function (json) {
+                json.data.name = api.createGuid();
+                json.data.version = 0;
+                return json.data;
+            }
+        });
+    });
+
+    it('00305: Should get a list of category nodes.', function () {
+        var url = '/bcms-api/categorytrees/' + constants.defaultCategoryTreeId + '/nodes',
             result,
             ready = false,
             data = {
                 filter: {
+                    connector: 'or',
                     where: [
-                        { field: 'Id', value: 'A60E08C1-9150-4DBE-BD32-A20601185796' },
-                        { field: 'CreatedOn', value: '2013-07-25 17:00:41.000' },
-                        { field: 'CreatedBy', value: 'Better CMS test user' },
-                        { field: 'LastModifiedOn', value: '2013-07-25 17:00:41.000' },
-                        { field: 'LastModifiedBy', value: 'Better CMS test user' },
-                        { field: 'Version', value: '1' },
-                        { field: 'Name', value: '00302' }
+                        { field: 'ParentId', value: constants.categoryNodeId }
                     ]
-                }
+                },
+                order: {
+                    by: [{ field: 'Name', direction: 'desc' }]
+                },
+                take: 2
             };
+
+        runs(function () {
+            api.get(url, data, function (json) {
+                result = json;
+                ready = true;
+            });
+        });
+
+        waitsFor(function () {
+            return ready;
+        }, 'The ' + url + ' timeout.');
+
+        runs(function () {
+            expect(result).toBeDefinedAndNotNull('JSON object should be retrieved.');
+            expect(result.data).toBeDefinedAndNotNull('JSON data object should be retrieved.');
+            expect(result.data.items).not.toBeNull('JSON data.items object should be retrieved.');
+// TODO: update default category tree not to have one child node '_0001_ - 3'.
+            expect(result.data.totalCount).toBe(0, 'Total count should be 0.');
+            expect(result.data.items.length).toBe(0, 'Returned array length should be 2.');
+
+//            expect(result.data.items[0].name).toBe('_Tree_2_', 'Correctly filtered items[0].title should be retrieved.');
+//            expect(result.data.items[1].name).toBe('_Tree_1_1_1_', 'Correctly filtered items[1].title should be retrieved.');
+//
+//            var node = result.data.items[1];
+//            api.expectBasePropertiesAreNotNull(node);
+//            expect(node.parentId).toBe(constants.child11Id, 'Correctly filtered parentId should be retrieved.');
+//            expect(node.displayOrder).toBeDefinedAndNotNull('displayOrder should be retrieved.');
+        });
+    });
+
+    it('00306: Should get a list with one sitemap node, filtered by all available columns.', function () {
+        var url = '/bcms-api/categorytrees/' + constants.defaultCategoryTreeId + '/nodes',
+            result,
+            ready = false;
+
+        var data = {
+            filter: {
+                where: [
+                    { field: 'Id', value: constants.categoryNodeId },
+                    { field: 'CreatedOn', value: '2013-07-23 16:51:53.000' },
+                    { field: 'CreatedBy', value: 'Better CMS test user' },
+                    { field: 'LastModifiedOn', value: '2015-02-04 09:52:24.000' },
+                    { field: 'LastModifiedBy', value: 'admin' },
+                    { field: 'Version', value: '2' },
+
+                    { field: 'ParentId', value: null },
+                    { field: 'Name', value: constants.categoryNodeName },
+                    { field: 'DisplayOrder', value: 5200 },
+                    { field: 'Macro', value: null },
+                ]
+            }
+        };
 
         runs(function () {
             api.get(url, data, function (json) {
@@ -107,22 +248,21 @@ describe('root.categories.api.behavior', function () {
             expect(result.data.totalCount).toBe(1, 'Total count should be 1.');
             expect(result.data.items.length).toBe(1, 'Returned array length should be 1.');
 
-            expect(result.data.items[0].id).toBe('a60e08c191504dbebd32a20601185796', 'Correctly filtered id should be retrieved.');
-
+            expect(result.data.items[0].id).toBe(constants.categoryNodeId, 'Correctly filtered id should be retrieved.');
+            
             // Check if model properties count didn't changed. If so - update current test filter and another tests.
-            // Can't understand this test :)
-            //expect(data.filter.where.length).toBe(api.getCountOfProperties(result.data.items[0]), 'Retrieved result properties cound should be equal to filterting parameters count.');
+            expect(data.filter.where.length).toBe(api.getCountOfProperties(result.data.items[0]), 'Retrieved result properties count should be equal to filter parameters count.');
         });
     });
-    
-    it('00303: Should throw validation exception for CategoryName/CategoryId, when getting category.', function () {
-        var url = '/bcms-api/categories/' + api.emptyGuid + '/?categoryName=test',
+
+    it('00307: Should get a category node by id.', function () {
+        var url = '/bcms-api/categorytrees/' + constants.defaultCategoryTreeId + '/nodes/' + constants.categoryNodeId,
             result,
             ready = false;
 
         runs(function () {
-            api.get(url, null, null, function (response) {
-                result = response.responseJSON;
+            api.get(url, null, function (json) {
+                result = json;
                 ready = true;
             });
         });
@@ -132,16 +272,26 @@ describe('root.categories.api.behavior', function () {
         }, 'The ' + url + ' timeout.');
 
         runs(function () {
-            api.expectValidationExceptionIsThrown(result, 'CategoryId');
+            expect(result).toBeDefinedAndNotNull('JSON object should be retrieved.');
+
+            var node = result.data;
+            expect(node).toBeDefinedAndNotNull('JSON data object should be retrieved.');
+            api.expectBasePropertiesAreNotNull(node);
+            expect(node.name).toBe(constants.categoryNodeName, 'Correctly filtered title should be retrieved.');
+            expect(node.displayOrder).toBeDefinedAndNotNull('displayOrder should be retrieved.');
         });
     });
 
-    it('00304: Should test CRUD for categories.', function () {
-        api.testCrud(runs, waitsFor, expect, "e87bfb18cdf74fd3a5dfa2040115ed1d", "/bcms-api/categories/", {
+    it('00308: Should test CRUD for category nodes.', function () {
+        api.testCrud(runs, waitsFor, expect, constants.categoryNodeId, '/bcms-api/categorytrees/' + constants.defaultCategoryTreeId + '/nodes/', {
+            getGetData: function () {
+                return {
+                    
+                }
+            },
             getPostData: function (json) {
                 json.data.name = api.createGuid();
                 json.data.version = 0;
-                json.data.categoryTreeId = '98FD87B4A25C4DDE933C83826B6A94D7';
                 return json.data;
             }
         });
