eb872fe59229a4dcb774a8b7a061e88d9fad7767
Simonas Mikulenas <simonas@devbridge.com>
2015-02-12 17:37:36 +0200

    Fixed missing to string override.

--- Modules/BetterCms.Module.Root/ViewModels/Category/CategorySuggestionViewModel.cs
+++ Modules/BetterCms.Module.Root/ViewModels/Category/CategorySuggestionViewModel.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-using BetterCms.Module.Root.ViewModels.Autocomplete;
+﻿using BetterCms.Module.Root.ViewModels.Autocomplete;
 
 namespace BetterCms.Module.Root.ViewModels.Category
 {
     public class CategorySuggestionViewModel : SuggestionViewModel
     {
         public string CategoryTreeForKey { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("CategoryTreeForKey: {0}", CategoryTreeForKey);
+        }
     }
 }
