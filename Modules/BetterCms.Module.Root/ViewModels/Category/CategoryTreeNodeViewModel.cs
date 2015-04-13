8de30de10ffaf0c88aa77d78a539f67f84044737
Augustas Adomavičius <augustas.adomavicius@devbridge.com>
2015-04-13 11:40:30 +0300

    #1200 Validate against HTML in various properties

--- Modules/BetterCms.Module.Root/ViewModels/Category/CategoryTreeNodeViewModel.cs
+++ Modules/BetterCms.Module.Root/ViewModels/Category/CategoryTreeNodeViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 using BetterCms.Module.Root.Content.Resources;
+using BetterCms.Module.Root.Mvc.Attributes;
 
 using BetterModules.Core.Models;
 
@@ -34,6 +35,7 @@ namespace BetterCms.Module.Root.ViewModels.Category
         /// <value>
         /// The title.
         /// </value>
+        [DisallowHtml(ErrorMessageResourceType = typeof(RootGlobalization), ErrorMessageResourceName = "Validation_DisallowHtml_Title_Message")]
         [StringLength(MaxLength.Name, ErrorMessageResourceType = typeof(RootGlobalization), ErrorMessageResourceName = "Validation_StringLengthAttribute_Message")]
         [Required(AllowEmptyStrings = false, ErrorMessageResourceType = typeof(RootGlobalization), ErrorMessageResourceName = "CategoryTree_Dialog_NodeTitle_RequiredMessage")]
         public string Title { get; set; }
