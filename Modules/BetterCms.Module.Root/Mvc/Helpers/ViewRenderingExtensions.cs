8f8945b373801b6dee38ffe031589b016c7e44a7
juliusSenkus <julius.senkus@devbridge.com>
2015-08-10 15:16:28 +0300

    Finished issue in code there was field unfilled, issue #1257

--- Modules/BetterCms.Module.Root/Mvc/Helpers/ViewRenderingExtensions.cs
+++ Modules/BetterCms.Module.Root/Mvc/Helpers/ViewRenderingExtensions.cs
@@ -138,6 +138,7 @@ namespace BetterCms.Module.Root.Mvc.Helpers
             newModel.CreatedByUser = pageModel.CreatedByUser;
             newModel.ModifiedByUser = pageModel.ModifiedByUser;
             newModel.IsMasterPage = pageModel.IsMasterPage;
+            newModel.LanguageCode = pageModel.LanguageCode;
 
             PopulateCollections(newModel, pageModel);
 
