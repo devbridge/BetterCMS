cf9eac413482fb6816e0e9f8739bcccaf61b5baf
Lukas Gerbenis <lukas.gerbenis@devbridge.com>
2015-10-15 17:36:30 +0300

    #1277 Created BcmsAuthorizeRequirement

--- vNext/BetterCms/src/BetterCms.Core/Security/BcmsAuthorizeAttribute.cs
+++ vNext/BetterCms/src/BetterCms.Core/Security/BcmsAuthorizeAttribute.cs
@@ -1,69 +1,69 @@
-﻿using System;
-using System.Net;
+﻿//using System;
+//using System.Net;
 
-using BetterCms.Core.Services;
+//using BetterCms.Core.Services;
 
-using Microsoft.AspNet.Authorization;
-using Microsoft.AspNet.Http;
-using Microsoft.AspNet.Mvc;
-using AuthorizationContext = Microsoft.AspNet.Authorization.AuthorizationContext;
+//using Microsoft.AspNet.Authorization;
+//using Microsoft.AspNet.Http;
+//using Microsoft.AspNet.Mvc;
+//using AuthorizationContext = Microsoft.AspNet.Authorization.AuthorizationContext;
 
-namespace BetterCms.Core.Security
-{
-    /// <summary>
-    /// Authorization attribute.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
-    public class BcmsAuthorizeAttribute : AuthorizeAttribute
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="BcmsAuthorizeAttribute"/> class.
-        /// </summary>
-        /// <param name="roles">The roles.</param>
-        public BcmsAuthorizeAttribute(params string[] roles)
-        {
-            Roles = string.Join(",", roles);
-        }
+//namespace BetterCms.Core.Security
+//{
+//    /// <summary>
+//    /// Authorization attribute.
+//    /// </summary>
+//    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+//    public class BcmsAuthorizeAttribute : AuthorizeAttribute
+//    {
+//        /// <summary>
+//        /// Initializes a new instance of the <see cref="BcmsAuthorizeAttribute"/> class.
+//        /// </summary>
+//        /// <param name="roles">The roles.</param>
+//        public BcmsAuthorizeAttribute(params string[] roles)
+//        {
+//            Roles = string.Join(",", roles);
+//        }
 
-        /// <summary>
-        /// When overridden, provides an entry point for custom authorization checks.
-        /// </summary>
-        /// <param name="httpContext">The HTTP context, which encapsulates all HTTP-specific information about an individual HTTP request.</param>
-        /// <returns>
-        /// true if the user is authorized; otherwise, false.
-        /// </returns>
-        /// <exception cref="System.ArgumentNullException">if httpContext is null.</exception>
-        protected override bool AuthorizeCore(HttpContext httpContext)
-        {
-            if (httpContext == null)
-            {
-                throw new ArgumentNullException(nameof(httpContext));
-            }
+//        /// <summary>
+//        /// When overridden, provides an entry point for custom authorization checks.
+//        /// </summary>
+//        /// <param name="httpContext">The HTTP context, which encapsulates all HTTP-specific information about an individual HTTP request.</param>
+//        /// <returns>
+//        /// true if the user is authorized; otherwise, false.
+//        /// </returns>
+//        /// <exception cref="System.ArgumentNullException">if httpContext is null.</exception>
+//        protected override bool AuthorizeCore(HttpContext httpContext)
+//        {
+//            if (httpContext == null)
+//            {
+//                throw new ArgumentNullException(nameof(httpContext));
+//            }
 
-            var container = PerWebRequestContainerProvider.GetLifetimeScope(httpContext);
-            if (container != null && container.IsRegistered<ISecurityService>())
-            {
-                var security = container.Resolve<ISecurityService>();
-                return security.IsAuthorized(httpContext.User, Roles);
-            }
+//            var container = PerWebRequestContainerProvider.GetLifetimeScope(httpContext);
+//            if (container != null && container.IsRegistered<ISecurityService>())
+//            {
+//                var security = container.Resolve<ISecurityService>();
+//                return security.IsAuthorized(httpContext.User, Roles);
+//            }
 
-            return base.AuthorizeCore(httpContext);
-        }
+//            return base.AuthorizeCore(httpContext);
+//        }
 
-        /// <summary>
-        /// Processes HTTP requests that fail authorization.
-        /// </summary>
-        /// <param name="filterContext">Encapsulates the information for using <see cref="T:System.Web.Mvc.AuthorizeAttribute" />. The <paramref name="filterContext" /> object contains the controller, HTTP context, request context, action result, and route data.</param>
-        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
-        {
-            if (filterContext.HttpContext.Request.IsAuthenticated)
-            {
-                filterContext.Result = new HttpStatusCodeResult((int)HttpStatusCode.Forbidden);
-            }
-            else
-            {
-                base.HandleUnauthorizedRequest(filterContext);
-            }
-        }
-    }
-}
+//        /// <summary>
+//        /// Processes HTTP requests that fail authorization.
+//        /// </summary>
+//        /// <param name="filterContext">Encapsulates the information for using <see cref="T:System.Web.Mvc.AuthorizeAttribute" />. The <paramref name="filterContext" /> object contains the controller, HTTP context, request context, action result, and route data.</param>
+//        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+//        {
+//            if (filterContext.HttpContext.Request.IsAuthenticated)
+//            {
+//                filterContext.Result = new HttpStatusCodeResult((int)HttpStatusCode.Forbidden);
+//            }
+//            else
+//            {
+//                base.HandleUnauthorizedRequest(filterContext);
+//            }
+//        }
+//    }
+//}
---
+++ vNext/BetterCms/src/BetterCms.Core/Security/BmsAuthorizeRequirement.cs
@@ -0,0 +1,25 @@
+﻿using BetterCms.Core.Services;
+using Microsoft.AspNet.Authorization;
+
+namespace BetterCms.Core.Security
+{
+    public class BmsAuthorizeRequirement: AuthorizationHandler<BmsAuthorizeRequirement>, IAuthorizationRequirement
+    {
+        private readonly ISecurityService securityService;
+        private readonly string roles;
+
+        public BmsAuthorizeRequirement(ISecurityService securityService, params string[] roles)
+        {
+            this.securityService = securityService;
+            this.roles = string.Join(",", roles);
+        }
+
+        protected override void Handle(AuthorizationContext context, BmsAuthorizeRequirement requirement)
+        {
+            if (securityService.IsAuthorized(context.User, roles))
+            {
+                context.Succeed(requirement);
+            }
+        }
+    }
+}
